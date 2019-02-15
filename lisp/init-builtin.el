52ed9844303697dccc089e9679c5afc8ba133141
Peter-Chou <zhou_00009@126.com>
2019-02-15 22:46:25 +0800

    update

--- lisp/init-builtin.el
+++ lisp/init-builtin.el
@@ -14,6 +14,44 @@
   (global-linum-mode 1))
 
 ;; Basic modes
-(recentf-mode 1)
+
+;; Start server
+(use-package server
+  :ensure nil
+  :hook (after-init . server-mode))
+
+  ;; History
+(use-package saveplace
+  :ensure nil
+  :hook (after-init . save-place-mode))
+
+(use-package recentf
+  :ensure nil
+  :hook (after-init . recentf-mode)
+  :init
+  (setq recentf-max-saved-items 200)
+  (setq recentf-exclude '((expand-file-name package-user-dir)
+                          ".cache"
+                          ".cask"
+                          ".elfeed"
+                          "bookmarks"
+                          "cache"
+                          "ido.*"
+                          "persp-confs"
+                          "recentf"
+                          "url"
+                          "COMMIT_EDITMSG\\'")))
+
+(use-package savehist
+  :ensure nil
+  :hook (after-init . savehist-mode)
+  :init (setq enable-recursive-minibuffers t ; Allow commands in minibuffers
+              history-length 1000
+              savehist-additional-variables '(mark-ring
+                                              global-mark-ring
+                                              search-ring
+                                              regexp-search-ring
+                                              extended-command-history)
+              savehist-autosave-interval 300))
 
 (provide 'init-builtin)
