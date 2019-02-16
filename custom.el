37b7fd46a6dc1f3f6e0e6e37e3f749f8c3a23543
Peter-Chou <zhou_00009@126.com>
2019-02-16 22:11:00 +0800

    :lipstick:

--- custom.el
+++ custom.el
@@ -3,10 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key evil paradox diminish use-package))))
+ '(custom-safe-themes
+   (quote
+    ("f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" default)))
+ '(package-selected-packages
+   (quote
+    (popwin gitignore-mode gitconfig-mode gitattributes-mode browse-at-remote git-messenger git-timemachine magit-todos aggressive-indent magit pinyinlib counsel-tramp counsel-world-clock counsel-projectile flyspell-correct-ivy ivy-xref ivy-rich ivy-yasnippet ivy-hydra amx flx counsel winum hlinum default-text-scale evil-anzu evil-nerd-commenter which-key evil paradox diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(linum-highlight-face ((t (:inherit (quote default) :background "#282a36" :foreground "#f8f8f2")))))
