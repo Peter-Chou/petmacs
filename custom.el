83ef54db4e4ab412fcaa3bd47339ada5ccf7600c
Peter-Chou <zhou_00009@126.com>
2019-02-17 21:06:54 +0800

    :lipstick:

--- custom.el
+++ custom.el
@@ -3,9 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" default)))
  '(package-selected-packages
    (quote
     (evil-magit editorconfig diffview daemons esup olivetti company-anaconda anaconda-mode yapfify smex popwin gitignore-mode gitconfig-mode gitattributes-mode browse-at-remote git-messenger git-timemachine magit-todos aggressive-indent magit pinyinlib counsel-tramp counsel-world-clock counsel-projectile flyspell-correct-ivy ivy-xref ivy-rich ivy-yasnippet ivy-hydra amx flx counsel winum hlinum default-text-scale evil-anzu evil-nerd-commenter which-key evil paradox diminish use-package))))
