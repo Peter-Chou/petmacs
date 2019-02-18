c38918b1d27ae1c9dfa0111b83d024c03d0df739
Peter-Chou <zhou_00009@126.com>
2019-02-18 08:53:17 +0800

    :lipstick:

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-magit editorconfig diffview daemons esup olivetti company-anaconda anaconda-mode yapfify smex popwin gitignore-mode gitconfig-mode gitattributes-mode browse-at-remote git-messenger git-timemachine magit-todos aggressive-indent magit pinyinlib counsel-tramp counsel-world-clock counsel-projectile flyspell-correct-ivy ivy-xref ivy-rich ivy-yasnippet ivy-hydra amx flx counsel winum hlinum default-text-scale evil-anzu evil-nerd-commenter which-key evil paradox diminish use-package))))
+    (ccls company-lsp lsp-ui lsp-mode evil-magit editorconfig diffview daemons esup olivetti company-anaconda anaconda-mode yapfify smex popwin gitignore-mode gitconfig-mode gitattributes-mode browse-at-remote git-messenger git-timemachine magit-todos aggressive-indent magit pinyinlib counsel-tramp counsel-world-clock counsel-projectile flyspell-correct-ivy ivy-xref ivy-rich ivy-yasnippet ivy-hydra amx flx counsel winum hlinum default-text-scale evil-anzu evil-nerd-commenter which-key evil paradox diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
