21b490260757afd80a10f67de439c508e153402a
Peter-Chou <zhou_00009@126.com>
2019-02-20 22:36:06 +0800

    :lipstick:

--- custom.el
+++ custom.el
@@ -5,10 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ibuffer-projectile rg company-box company quelpa-use-package quelpa vim-empty-lines-mode yasnippet-snippets yasnippet ccls company-lsp lsp-ui lsp-mode evil-magit editorconfig diffview daemons esup olivetti company-anaconda anaconda-mode yapfify smex popwin gitignore-mode gitconfig-mode gitattributes-mode browse-at-remote git-messenger git-timemachine magit-todos aggressive-indent magit pinyinlib counsel-tramp counsel-world-clock counsel-projectile flyspell-correct-ivy ivy-xref ivy-rich ivy-yasnippet ivy-hydra amx flx counsel winum hlinum default-text-scale evil-anzu evil-nerd-commenter which-key evil paradox diminish use-package))))
+    (beacon smart-semicolon carbon-now-sh symbol-overlay elisp-refs avy restart-emacs ibuffer-projectile rg company-box company quelpa-use-package quelpa vim-empty-lines-mode yasnippet-snippets yasnippet ccls company-lsp lsp-ui lsp-mode evil-magit editorconfig diffview daemons esup olivetti company-anaconda anaconda-mode yapfify smex popwin gitignore-mode gitconfig-mode gitattributes-mode browse-at-remote git-messenger git-timemachine magit-todos aggressive-indent magit pinyinlib counsel-tramp counsel-world-clock counsel-projectile flyspell-correct-ivy ivy-xref ivy-rich ivy-yasnippet ivy-hydra amx flx counsel winum hlinum default-text-scale evil-anzu evil-nerd-commenter which-key evil paradox diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(hl-todo ((t (:box t :bold t))))
  '(linum-highlight-face ((t (:inherit (quote default) :background "#282a36" :foreground "#f8f8f2")))))
