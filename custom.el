a3d1787a3b0c5ecf0cf21c8beddfdd9bce6f58fb
Peter-Chou <zhou_00009@126.com>
2019-02-21 18:59:18 +0800

    :lipstick:

--- custom.el
+++ custom.el
@@ -5,11 +5,16 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (beacon smart-semicolon carbon-now-sh symbol-overlay elisp-refs avy restart-emacs ibuffer-projectile rg company-box company quelpa-use-package quelpa vim-empty-lines-mode yasnippet-snippets yasnippet ccls company-lsp lsp-ui lsp-mode evil-magit editorconfig diffview daemons esup olivetti company-anaconda anaconda-mode yapfify smex popwin gitignore-mode gitconfig-mode gitattributes-mode browse-at-remote git-messenger git-timemachine magit-todos aggressive-indent magit pinyinlib counsel-tramp counsel-world-clock counsel-projectile flyspell-correct-ivy ivy-xref ivy-rich ivy-yasnippet ivy-hydra amx flx counsel winum hlinum default-text-scale evil-anzu evil-nerd-commenter which-key evil paradox diminish use-package))))
+    (fill-column-indicator diredfl all-the-icons-dired dired-quick-sort shell-pop esh-autosuggest eshell-z esh-help eshell-prompt-extras virtualenvwrapper beacon smart-semicolon carbon-now-sh symbol-overlay elisp-refs avy restart-emacs ibuffer-projectile rg company-box company quelpa-use-package quelpa vim-empty-lines-mode yasnippet-snippets yasnippet ccls company-lsp lsp-ui lsp-mode evil-magit editorconfig diffview daemons esup olivetti company-anaconda anaconda-mode yapfify smex popwin gitignore-mode gitconfig-mode gitattributes-mode browse-at-remote git-messenger git-timemachine magit-todos aggressive-indent magit pinyinlib counsel-tramp counsel-world-clock counsel-projectile flyspell-correct-ivy ivy-xref ivy-rich ivy-yasnippet ivy-hydra amx flx counsel winum hlinum default-text-scale evil-anzu evil-nerd-commenter which-key evil paradox diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(all-the-icons-dired-dir-face ((t (\` (:foreground (\, (face-background (quote default))))))))
+ '(epe-pipeline-delimiter-face ((t (:foreground "#fd780f" :weight bold))))
+ '(epe-pipeline-host-face ((t (:foreground "#3cd8a2" :weight bold))))
+ '(epe-pipeline-time-face ((t (:foreground "#e2c504"))))
+ '(epe-pipeline-user-face ((t (:foreground "#ef2d2d" :weight bold))))
  '(hl-todo ((t (:box t :bold t))))
  '(linum-highlight-face ((t (:inherit (quote default) :background "#282a36" :foreground "#f8f8f2")))))
