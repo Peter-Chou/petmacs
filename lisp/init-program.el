8a7a027ae2656924606823531668d756df9fe284
Peter-Chou <zhou_00009@126.com>
2019-02-22 21:09:59 +0800

    add pretty-* packages

--- lisp/init-program.el
+++ lisp/init-program.el
@@ -43,4 +43,56 @@
   (evil-define-key 'normal imenu-list-major-mode-map (kbd "r") 'imenu-list-refresh) 
   (evil-define-key 'normal imenu-list-major-mode-map (kbd "q") 'imenu-list-quit-window))
 
+
+;; (use-package prettify-utils) ; needed by pretty-code
+
+(use-package prettify-utils
+  :ensure t
+  :quelpa
+  (prettify-utils :repo "Ilazki/prettify-utils.el" :fetcher github))
+
+(use-package pretty-code
+  :ensure nil
+  :defer nil
+  :init
+  (progn
+    (require 'pretty-code)
+    (pretty-code-add-hook 'emacs-lisp-mode-hook '((:def "defun")))
+    (pretty-code-add-hook 'python-mode-hook     '((:def "def")
+					          (:lambda "lambda")))))
+
+(use-package pretty-fonts
+  :ensure nil
+  :defer nil
+  :hook (after-make-frame-functions . petmacs/complete-setup-pretty-code)
+  :preface
+  (defun petmacs/complete-setup-pretty-code ()
+    (require 'pretty-fonts)
+    (pretty-fonts-add-hook 'prog-mode-hook pretty-fonts-fira-code-alist)
+    (pretty-fonts-add-hook 'org-mode-hook  pretty-fonts-fira-code-alist)
+
+    (pretty-fonts-set-fontsets-for-fira-code)
+    (pretty-fonts-set-fontsets
+     '(;; All-the-icons fontsets
+       ("fontawesome"
+	;;                         
+	#xf07c #xf0c9 #xf0c4 #xf0cb #xf017 #xf101)
+
+       ("all-the-icons"
+	;;    
+	#xe907 #xe928)
+
+       ("github-octicons"
+	;;                               
+	#xf091 #xf059 #xf076 #xf075 #xe192  #xf016 #xf071)
+
+       ("material icons"
+	;;              
+	#xe871 #xe918 #xe3e7  #xe5da
+	;;              
+	#xe3d0 #xe3d1 #xe3d2 #xe3d4)))
+    )
+  :init
+  (petmacs/complete-setup-pretty-code))
+
 (provide 'init-program)
