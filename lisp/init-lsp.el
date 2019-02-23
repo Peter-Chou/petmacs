b54575f721b57b5ebdfdcb710c3b31367c03b379
Peter-Chou <zhou_00009@126.com>
2019-02-23 21:31:21 +0800

    setup lsp-ui-imenu evil keybindings

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -17,7 +17,15 @@
   :bind (:map lsp-ui-mode-map
               ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
               ([remap xref-find-references] . lsp-ui-peek-find-references)
-              ("C-c u" . lsp-ui-imenu)))
+              ("C-c u" . lsp-ui-imenu))
+  :init
+  (evil-define-key 'normal lsp-ui-imenu-mode-map (kbd "q") 'lsp-ui-imenu--kill)
+  (evil-define-key 'normal lsp-ui-imenu-mode-map (kbd "j") 'lsp-ui-imenu--next-kind)
+  (evil-define-key 'normal lsp-ui-imenu-mode-map (kbd "k") 'lsp-ui-imenu--prev-kind)
+  (evil-define-key 'normal lsp-ui-imenu-mode-map (kbd "<return>") 'lsp-ui-imenu--visit)
+  (evil-define-key 'normal lsp-ui-imenu-mode-map (kbd "d") 'lsp-ui-imenu--view)
+  )
+
 
 (use-package company-lsp)
 
