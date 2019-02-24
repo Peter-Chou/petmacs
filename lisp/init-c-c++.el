144e4c06775c3f48a0d622cd80ee4fb9dbe6a4ba
Peter-Chou <zhou_00009@126.com>
2019-02-24 13:40:49 +0800

    update

--- lisp/init-c-c++.el
+++ lisp/init-c-c++.el
@@ -8,23 +8,23 @@
 (use-package ccls
   :defines projectile-project-root-files-top-down-recurring
   :hook ((c-mode c++-mode objc-mode cuda-mode) . (lambda ()
-                                                   (require 'ccls)
-                                                   (lsp)))
+                                                  (require 'ccls)
+                                                  (lsp)))
   :init
   (setq ccls-executable (file-truename "~/ccls/Release/ccls"))
   (setq ccls-initialization-options
-	(if (boundp 'ccls-initialization-options)
-	    (append ccls-initialization-options `(:cacheDirectory ,(expand-file-name "~/.ccls-cache")))
-	  `(:cacheDirectory ,(expand-file-name "~/.ccls-cache"))))
+  	(if (boundp 'ccls-initialization-options)
+  	    (append ccls-initialization-options `(:cacheDirectory ,(expand-file-name "~/.ccls-cache")))
+  	  `(:cacheDirectory ,(expand-file-name "~/.ccls-cache"))))
 
   (setq ccls-sem-highlight-method 'overlay)
 
   :config
   (with-eval-after-load 'projectile
     (setq projectile-project-root-files-top-down-recurring
-	  (append '("compile_commands.json"
-		    ".ccls")
-		  projectile-project-root-files-top-down-recurring))))
+  	  (append '("compile_commands.json"
+  		    ".ccls")
+  		  projectile-project-root-files-top-down-recurring))))
 
 (use-package smart-semicolon
   :defer t
