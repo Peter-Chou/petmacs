3aba4646342894c9716b63015188004179e80536
Peter-Chou <zhou_00009@126.com>
2019-03-02 08:19:49 +0800

    disable emacs-lisp-checkdoc

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -14,6 +14,7 @@
   :config
   (setq flycheck-indication-mode 'right-fringe)
   (setq flycheck-emacs-lisp-load-path 'inherit)
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 
   ;; Only check while saving and opening files
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
