a3538487c0db5f889beeb3a8179f2163f7432c1e
Peter-Chou <zhou_00009@126.com>
2019-03-02 20:20:22 +0800

    add init-variable.el

---
+++ lisp/init-variable.el
@@ -0,0 +1,13 @@
+;; init-variable.el --- Define petmacs vaiables.	-*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(defvar petmacs-default-language-env 'UTF-8)
+
+(defvar petmacs-default-coding-env 'utf-8)
+
+(provide 'init-variable)
+
+;;; init-const.el ends here
