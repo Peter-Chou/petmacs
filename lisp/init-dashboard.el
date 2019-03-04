51426a94095ae54fa02b4b9ef451db69383b1ce7
Peter-Chou <zhou_00009@126.com>
2019-03-04 20:23:56 +0800

    use dashboard default face setting

--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -12,6 +12,8 @@
   :init
   (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
   (setq dashboard-banner-logo-title "Petmacs --- Adorable just like A PET")
+  (setq dashboard-show-shortcuts nil)
+  (setq show-week-agenda-p t)
   (setq dashboard-startup-banner (expand-file-name "img/totoro_banner.png" user-emacs-directory))
   (setq dashboard-items '((recents  . 8)
                           (bookmarks . 5)
@@ -20,11 +22,10 @@
   :hook ((after-init . dashboard-setup-startup-hook)
          (dashboard-mode . (lambda () (setq-local frame-title-format ""))))
   :custom-face
-  (dashboard-banner-logo-title-face ((t (:inherit bold))))
-  (dashboard-heading-face ((t (:inherit (font-lock-keyword-face bold)))))
+  ;; (dashboard-banner-logo-title-face ((t (:inherit bold))))
+  ;; (dashboard-heading-face ((t (:inherit (font-lock-keyword-face bold)))))
   :config
   (dashboard-setup-startup-hook)
-  (setq show-week-agenda-p t)
   (evil-define-key 'normal dashboard-mode-map (kbd "RET") 'widget-button-press)
   (evil-define-key 'normal dashboard-mode-map (kbd "gd") 'widget-button-press)
   (evil-define-key 'normal dashboard-mode-map [down-mouse-1] 'widget-button-click)
