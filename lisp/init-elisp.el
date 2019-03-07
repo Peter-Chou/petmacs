9c09ccb39ef9a62501915d92c0bbd56925316695
Peter-Chou <zhou_00009@126.com>
2019-03-07 21:34:51 +0800

    remove eldoc-box

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -6,26 +6,9 @@
 
 ;; Show function arglist or variable docstring
 ;; `global-eldoc-mode' is enabled by default.
-;; (use-package eldoc
-;;   :ensure nil
-;;   :diminish eldoc-mode)
 (use-package eldoc
   :ensure nil
-  :diminish
-  :config
-  (when emacs/>=26p
-    ;; Display documentation in childframe
-    (use-package eldoc-box
-      :diminish
-      :hook ((eldoc-mode . (lambda ()
-                             ;; Compatible with `lsp-mode'
-                             (unless (bound-and-true-p lsp-mode)
-                               (eldoc-box-hover-mode 1)
-                               (eldoc-box-hover-at-point-mode 1))))
-             (lsp-mode . (lambda ()
-                           ;; Compatible with `lsp-mode'
-                           (if eldoc-box-hover-mode
-                               (eldoc-box-hover-mode -1))))))))
+  :diminish eldoc-mode)
 
 ;; Semantic code search for emacs lisp
 (use-package elisp-refs)
