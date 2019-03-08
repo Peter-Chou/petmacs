b30271aad526fba9785c4cda41f0b27eb0a4db3e
Peter-Chou <zhou_00009@126.com>
2019-03-08 18:18:50 +0800

    add init-sql.el

---
+++ lisp/init-sql.el
@@ -0,0 +1,32 @@
+;; init-sql.el --- Setup sql.  -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package sql
+  :ensure nil
+  :hook (sql-mode . (lambda ()
+		      (set (make-local-variable 'company-backends)
+			   '((company-capf company-files)))))
+  )
+
+(use-package sql-indent
+  :quelpa
+  (sql-indent :fetcher github
+  	          :repo "alex-hhh/emacs-sql-indent"
+  	          :files ("sql-indent.el"))
+  :hook (sql-mode . sqlind-minor-mode))
+
+(use-package sqlup-mode
+  :hook ((sql-mode . sqlup-mode)
+	 (sql-interactive-mode . sqlup-mode))
+  :config
+  (setq sqlup-blacklist (append sqlup-blacklist
+                                sql-capitalize-keywords-blacklist))
+  ;; TODO: add company backends
+  )
+
+(provide 'init-sql)
+
+;;; init-sql.el ends here
