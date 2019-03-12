4034d30ddb8d9e61adaf809e99449c52506d7771
Peter-Chou <zhou_00009@126.com>
2019-03-12 20:54:55 +0800

    use company-box built-in face

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -45,63 +45,7 @@
     :hook (company-mode . company-box-mode)
     :init (setq company-box-enable-icon (display-graphic-p))
     :config
-    (setq company-box-backends-colors nil)
-
-    (with-eval-after-load 'all-the-icons
-      (eval-and-compile
-        (defun petmacs//company-box-icon (family icon &rest args)
-          "Defines icons using `all-the-icons' for `company-box'."
-          (when icon
-            (let ((icon (pcase family
-                          ('octicon (all-the-icons-octicon icon :v-adjust -0.05 args))
-                          ('faicon (all-the-icons-faicon icon :v-adjust -0.0575))
-                          ('material (all-the-icons-material icon :v-adjust -0.225 args))
-                          ('alltheicon (all-the-icons-alltheicon icon args)))))
-              (unless (symbolp icon)
-                (concat icon
-                        (propertize " " 'face 'variable-pitch)))))))
-
-      (setq company-box-icons-unknown
-            (petmacs//company-box-icon 'octicon "file-text"))
-
-      (setq company-box-icons-elisp
-            (list
-             (petmacs//company-box-icon 'faicon "cube")        ; Function
-             (petmacs//company-box-icon 'faicon "tag")         ; Variable
-             (petmacs//company-box-icon 'faicon "cog")         ; Feature
-             (petmacs//company-box-icon 'material "palette")   ; Face
-             ))
-
-      (setq company-box-icons-yasnippet
-            (petmacs//company-box-icon 'octicon "file-code"))  ; Snippet
-
-      (setq company-box-icons-lsp
-            `(( 1  . ,(petmacs//company-box-icon 'faicon "file-text-o"))     ; Text
-              ( 2  . ,(petmacs//company-box-icon 'faicon "cube"))            ; Method
-              ( 3  . ,(petmacs//company-box-icon 'faicon "cube"))            ; Function
-              ( 4  . ,(petmacs//company-box-icon 'faicon "cube"))            ; Constructor
-              ( 5  . ,(petmacs//company-box-icon 'faicon "tag"))             ; Field
-              ( 6  . ,(petmacs//company-box-icon 'faicon "tag"))             ; Variable
-              ( 7  . ,(petmacs//company-box-icon 'faicon "cog"))             ; Class
-              ( 8  . ,(petmacs//company-box-icon 'faicon "cogs"))            ; Interface
-              ( 9  . ,(petmacs//company-box-icon 'alltheicon "less"))        ; Module
-              (10  . ,(petmacs//company-box-icon 'faicon "wrench"))          ; Property
-              (11  . ,(petmacs//company-box-icon 'faicon "tag"))             ; Unit
-              (12  . ,(petmacs//company-box-icon 'faicon "tag"))             ; Value
-              (13  . ,(petmacs//company-box-icon 'faicon "file-text-o"))     ; Enum
-              (14  . ,(petmacs//company-box-icon 'material "format_align_center")) ; Keyword
-              (15  . ,(petmacs//company-box-icon 'material "content_paste")) ; Snippet
-              (16  . ,(petmacs//company-box-icon 'material "palette"))       ; Color
-              (17  . ,(petmacs//company-box-icon 'faicon "file"))            ; File
-              (18  . ,(petmacs//company-box-icon 'faicon "tag"))             ; Reference
-              (19  . ,(petmacs//company-box-icon 'faicon "folder"))          ; Folder
-              (20  . ,(petmacs//company-box-icon 'faicon "tag"))             ; EnumMember
-              (21  . ,(petmacs//company-box-icon 'faicon "tag"))             ; Constant
-              (22  . ,(petmacs//company-box-icon 'faicon "cog"))             ; Struct
-              (23  . ,(petmacs//company-box-icon 'faicon "bolt"))            ; Event
-              (24  . ,(petmacs//company-box-icon 'faicon "tag"))             ; Operator
-              (25  . ,(petmacs//company-box-icon 'faicon "cog"))             ; TypeParameter
-              )))))
+    (setq company-box-backends-colors nil)))
 
 (provide 'init-company)
 
