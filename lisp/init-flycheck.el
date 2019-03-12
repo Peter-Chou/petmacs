5da638788b1c65b6f2e411ebf756c65ef49f9d9e
Peter-Chou <zhou_00009@126.com>
2019-03-12 20:56:15 +0800

    remove flycheck-posframe

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -17,23 +17,7 @@
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 
   ;; Only check while saving and opening files
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-
-  ;; Display Flycheck errors in GUI tooltips
-  (if (display-graphic-p)
-      (if emacs/>=26p
-          (use-package flycheck-posframe
-            :hook (flycheck-mode . flycheck-posframe-mode))
-        (use-package flycheck-pos-tip
-          :defines flycheck-pos-tip-timeout
-          :hook (global-flycheck-mode . flycheck-pos-tip-mode)
-          :config (setq flycheck-pos-tip-timeout 30)))
-    (use-package flycheck-popup-tip
-      :hook (flycheck-mode . flycheck-popup-tip-mode)))
-
-  ;; Jump to and fix syntax errors via `avy'
-  (use-package avy-flycheck
-    :hook (global-flycheck-mode . avy-flycheck-setup)))
+  (setq flycheck-check-syntax-automatically '(save mode-enabled)))
 
 (provide 'init-flycheck)
 
