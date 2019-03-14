92ce267728a7103fe5cc5a5f37b98b71ce255e60
Peter-Chou <zhou_00009@126.com>
2019-03-14 19:57:27 +0800

    use original treemacs icon

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -54,12 +54,12 @@
   (doom-themes-enable-bold t)
   :config
   (progn
+    ;; enable custom treemacs themes
+    ;; (doom-themes-treemacs-config)
     ;; Enable flashing mode-line on errors
     (doom-themes-visual-bell-config)
     ;; Corrects (and improves) org-mode's native fontification.
-    (doom-themes-org-config)
-    ;; enable custom treemacs themes
-    (doom-themes-treemacs-config)))
+    (doom-themes-org-config)))
 
 (load-theme 'doom-dracula t)
 
