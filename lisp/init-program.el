bd1b472e6d55c82a7a7b0e077412df9bcbe955ca
Peter-Chou <zhou_00009@126.com>
2019-03-14 19:57:00 +0800

    disable ido-mode

--- lisp/init-program.el
+++ lisp/init-program.el
@@ -4,26 +4,6 @@
 
 ;;; Code:
 
-;; (setq electric-pair-inhibit-predicate 'electric-pair-conservative-inhibit)
-;; (electric-pair-mode 1)
-
-;; IDO
-(ido-mode 1)
-(ido-everywhere 1)
-(setq ido-use-virtual-buffers t)
-(setq ido-use-filename-at-point 'guess)
-(setq ido-create-new-buffer 'always)
-(setq ido-enable-flex-matching t)
-
-(defun ido-recentf-open ()
-  "Use `ido-completing-read' to find a recent file."
-  (interactive)
-  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
-      (message "Opening file...")
-    (message "Aborting")))
-
-(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
-
 (use-package fill-column-indicator
   :hook (prog-mode . (lambda ()
 		       (fci-mode 1)
@@ -56,49 +36,46 @@
 
 (use-package pretty-code
   :ensure nil
-  :defer nil
   :init
-  (progn
-    (require 'pretty-code)
-    ;; (pretty-code-add-hook 'python-mode-hook     '((:def "def")
-    ;; 					          (:lambda "lambda")))
-    (pretty-code-add-hook 'emacs-lisp-mode-hook '((:def "defun")
-						  (:lambda "lambda")))
-    ))
+  (require 'pretty-code)
+  (pretty-code-add-hook 'python-mode-hook     '((:def "def")
+    					        (:lambda "lambda")))
+  (pretty-code-add-hook 'emacs-lisp-mode-hook '((:def "defun")
+						(:lambda "lambda"))))
 
 (use-package pretty-fonts
-  :ensure nil
-  :defer nil
-  :hook (after-make-frame-functions . petmacs/complete-setup-pretty-code)
-  :preface
-  (defun petmacs/complete-setup-pretty-code ()
-    (require 'pretty-fonts)
-    (pretty-fonts-add-hook 'prog-mode-hook pretty-fonts-fira-code-alist)
-    (pretty-fonts-add-hook 'org-mode-hook  pretty-fonts-fira-code-alist)
+:ensure nil
+:defer nil
+:hook (after-make-frame-functions . petmacs/complete-setup-pretty-code)
+:preface
+(defun petmacs/complete-setup-pretty-code ()
+  (require 'pretty-fonts)
+  (pretty-fonts-add-hook 'prog-mode-hook pretty-fonts-fira-code-alist)
+  (pretty-fonts-add-hook 'org-mode-hook  pretty-fonts-fira-code-alist)
 
-    (pretty-fonts-set-fontsets-for-fira-code)
-    (pretty-fonts-set-fontsets
-     '(;; All-the-icons fontsets
-       ("fontawesome"
-	;;                         
-	#xf07c #xf0c9 #xf0c4 #xf0cb #xf017 #xf101)
+  (pretty-fonts-set-fontsets-for-fira-code)
+  (pretty-fonts-set-fontsets
+   '(;; All-the-icons fontsets
+     ("fontawesome"
+      ;;                         
+      #xf07c #xf0c9 #xf0c4 #xf0cb #xf017 #xf101)
 
-       ("all-the-icons"
-	;;    
-	#xe907 #xe928)
+     ("all-the-icons"
+      ;;    
+      #xe907 #xe928)
 
-       ("github-octicons"
-	;;                               
-	#xf091 #xf059 #xf076 #xf075 #xe192  #xf016 #xf071)
+     ("github-octicons"
+      ;;                               
+      #xf091 #xf059 #xf076 #xf075 #xe192  #xf016 #xf071)
 
-       ("material icons"
-	;;              
-	#xe871 #xe918 #xe3e7  #xe5da
-	;;              
-	#xe3d0 #xe3d1 #xe3d2 #xe3d4)))
-    )
-  :init
-  (petmacs/complete-setup-pretty-code))
+     ("material icons"
+      ;;              
+      #xe871 #xe918 #xe3e7  #xe5da
+      ;;              
+      #xe3d0 #xe3d1 #xe3d2 #xe3d4)))
+  )
+:init
+(petmacs/complete-setup-pretty-code))
 
 (use-package electric-operator
   :hook ((c-mode-common . electric-operator-mode)
