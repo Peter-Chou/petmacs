17538e48d2982b7a3857356931052883680a8a96
Peter-Chou <zhou_00009@126.com>
2019-03-15 19:49:28 +0800

    remove unused org mode packages

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,225 +1,59 @@
-;; init-org.el --- Setup org.  -*- lexical-binding: t -*-
+;; init-org.el --- Setup org mode.  -*- lexical-binding: t -*-
 
 ;;; Commentary:
 
 ;;; Code:
+(eval-when-compile
+  (require 'init-const)
+  (require 'init-variable))
 
 (use-package org
-  :ensure nil
-  :commands org-try-structure-completion
-  :functions hydra-org-template/body
-  :bind (("C-c a" . org-agenda)
-	 ("C-c c" . org-capture)
-         ("C-c b" . org-switchb))
-  :hook (org-indent-mode . (lambda() (diminish 'org-indent-mode)))
-  :config
-  (setq org-agenda-files '("~/org")
-        org-todo-keywords '((sequence "TODO(T)" "DOING(I)" "HANGUP(H)" "|" "DONE(D)" "CANCEL(C)")
-                            (sequence "⚑(t)" "🏴(i)" "❓(h)" "|" "✔(d)" "✘(c)"))
-        org-todo-keyword-faces '(("HANGUP" . warning)
-                                 ("❓" . warning))
-        org-log-done 'time
-        org-startup-indented t
-        org-ellipsis (if (char-displayable-p ?) "  " nil)
-        org-pretty-entities t
-        org-hide-emphasis-markers t)
-  (setq org-agenda-current-time-string "← now")
-  (setq org-agenda-time-grid ;; Format is changed from 9.1
-        '((daily today require-timed)
-          (0900 01000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000 2100 2200 2300 2400)
-          "-"
-	  "────────────────"))
-
-  (add-to-list 'org-export-backends 'md)
-
-  ;; More fancy UI
-  (use-package org-bullets
-    :if (char-displayable-p ?◉)
-    :hook (org-mode . org-bullets-mode))
-
-  (use-package org-fancy-priorities
-    :diminish
-    :defines org-fancy-priorities-list
-    :hook (org-mode . org-fancy-priorities-mode)
-    :config
-    (unless (char-displayable-p ?❗)
-      (setq org-fancy-priorities-list '("HIGH" "MID" "LOW" "OPTIONAL"))))
-
-  ;; Babel
-  (setq org-confirm-babel-evaluate nil
-        org-src-fontify-natively t
-        org-src-tab-acts-natively t)
-
-  (defvar load-language-list '((emacs-lisp . t)
-                               (perl . t)
-                               (python . t)
-                               (ruby . t)
-                               (js . t)
-                               (css . t)
-                               (sass . t)
-                               (C . t)
-                               (java . t)
-                               (plantuml . t)))
-
-  ;; ob-sh renamed to ob-shell since 26.1.
-  (if emacs/>=26p
-      (cl-pushnew '(shell . t) load-language-list)
-    (cl-pushnew '(sh . t) load-language-list))
-
-  (use-package ob-go
-    :init (cl-pushnew '(go . t) load-language-list))
-
-  (use-package ob-rust
-    :init (cl-pushnew '(rust . t) load-language-list))
-
-  (use-package ob-ipython
-    :if (executable-find "jupyter")     ; DO NOT remove
-    :init (cl-pushnew '(ipython . t) load-language-list))
-
-  (org-babel-do-load-languages 'org-babel-load-languages
-                               load-language-list)
-
-  ;; Rich text clipboard
-  (use-package org-rich-yank
-    :bind (:map org-mode-map
-                ("C-M-y" . org-rich-yank)))
-
-  ;; Table of contents
-  (use-package toc-org
-    :hook (org-mode . toc-org-mode))
-
-  ;; Preview
-  (use-package org-preview-html
-    :diminish org-preview-html-mode)
-
-  ;; Presentation
-  (use-package org-tree-slide
-    :diminish
-    :functions (org-display-inline-images
-                org-remove-inline-images)
-    :bind (:map org-mode-map
-                ("C-<f7>" . org-tree-slide-mode)
-                :map org-tree-slide-mode-map
-                ("<left>" . org-tree-slide-move-previous-tree)
-                ("<right>" . org-tree-slide-move-next-tree)
-                ("S-SPC" . org-tree-slide-move-previous-tree)
-                ("SPC" . org-tree-slide-move-next-tree))
-    :hook ((org-tree-slide-play . (lambda ()
-                                    (text-scale-increase 4)
-                                    (org-display-inline-images)
-                                    (read-only-mode 1)))
-           (org-tree-slide-stop . (lambda ()
-                                    (text-scale-increase 0)
-                                    (org-remove-inline-images)
-                                    (read-only-mode -1))))
-    :config
-    (org-tree-slide-simple-profile)
-    (setq org-tree-slide-skip-outline-level 2))
-
-  ;; Pomodoro
-  (use-package org-pomodoro
-    :after org-agenda
-    :bind (:map org-agenda-mode-map
-                ("P" . org-pomodoro)))
-
-  ;; Visually summarize progress
-  (use-package org-dashboard)
-
-  (eval-and-compile
-    (defun hot-expand (str &optional mod)
-      "Expand org template."
-      (let (text)
-        (when (region-active-p)
-          (setq text (buffer-substring (region-beginning) (region-end)))
-          (delete-region (region-beginning) (region-end)))
-        (insert str)
-        (org-try-structure-completion)
-        (when mod (insert mod) (forward-line))
-        (when text (insert text)))))
-
-  (defhydra hydra-org-template (:color blue :hint nil)
-    "
-_c_enter  qu_o_te     _e_macs-lisp    _L_aTeX:
-_l_atex   _E_xample   p_y_thon        _i_ndex:
-_a_scii   _v_erse     ip_Y_thon       _I_NCLUDE:
-_s_rc     _g_o        _r_uby          _H_TML:
-_h_tml    _S_HELL     _p_erl          _A_SCII:
-^ ^       ^ ^         _P_erl tangled  plant_u_ml
-"
-    ("s" (hot-expand "<s"))
-    ("E" (hot-expand "<e"))
-    ("o" (hot-expand "<q"))
-    ("v" (hot-expand "<v"))
-    ("c" (hot-expand "<c"))
-    ("l" (hot-expand "<l"))
-    ("h" (hot-expand "<h"))
-    ("a" (hot-expand "<a"))
-    ("L" (hot-expand "<L"))
-    ("i" (hot-expand "<i"))
-    ("e" (hot-expand "<s" "emacs-lisp"))
-    ("y" (hot-expand "<s" "python :results output"))
-    ("Y" (hot-expand "<s" "ipython :session :exports both :results raw drawer\n$0"))
-    ("g" (hot-expand "<s" "go :imports '\(\"fmt\"\)"))
-    ("p" (hot-expand "<s" "perl"))
-    ("r" (hot-expand "<s" "ruby"))
-    ("S" (hot-expand "<s" "sh"))
-    ("u" (hot-expand "<s" "plantuml :file CHANGE.png"))
-    ("P" (progn
-           (insert "#+HEADERS: :results output :exports both :shebang \"#!/usr/bin/env perl\"\n")
-           (hot-expand "<s" "perl")))
-    ("I" (hot-expand "<I"))
-    ("H" (hot-expand "<H"))
-    ("A" (hot-expand "<A"))
-    ("<" self-insert-command "ins")
-    ("q" nil "quit"))
-
-  (bind-key "<"
-            (lambda () (interactive)
-              (if (or (region-active-p) (looking-back "^\s*" 1))
-                  (hydra-org-template/body)
-                (self-insert-command 1)))
-            org-mode-map))
-
-
-(use-package evil-org
-  :preface
-  (defun petmacs//evil-org-mode ()
-    (evil-org-mode)
-    (evil-normalize-keymaps)
-    (evil-org-set-key-theme))
-  :hook (org-mode . petmacs//evil-org-mode)
+  :commands (orgtbl-mode)
   :init
-  (setq evil-org-use-additional-insert t
-        evil-org-set-key-theme '(textobjects insert navigation additional calendar shift todo heading))
-  :config
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
+  (require 'org)
+  (setq org-directory "~/org"
+        org-default-notes-file (expand-file-name "notes.org" org-directory)
+        org-log-done t
+        org-startup-with-inline-images t
+        org-image-actual-width nil
+        org-src-fontify-natively t
+        org-src-tab-acts-natively t
+        ;; this is consistent with the value of
+        ;; `helm-org-headings-max-depth'.
+        org-imenu-depth 8))
+
+(use-package org-agenda
+  :ensure nil
+  :init
+  (setq org-agenda-restore-windows-after-quit t)
+  )
+
+(use-package org-bullets
+  :if (char-displayable-p ?◉)
+  :hook (org-mode . org-bullets-mode))
+
+(use-package toc-org
+  :hook (org-mode . toc-org-enable)
+  :init
+  (setq toc-org-max-depth 10))
 
 (use-package org-projectile
+  :defer nil
   :commands (org-projectile-location-for-project)
-  :preface
-  (defun org-projectile/capture (&optional arg)
-    (interactive "P")
-    (if arg
-	(org-projectile-project-todo-completing-read :empty-lines 1)
-      (org-projectile-capture-for-current-project :empty-lines 1)))
-
-  (defun org-projectile/goto-todos ()
-    (interactive)
-    (org-projectile-goto-location-for-project (projectile-project-name)))
-
   :init
-  (defvar org-projectile-file "TODO.org")
   (with-eval-after-load 'org-capture
     (require 'org-projectile))
   :config
   (if (file-name-absolute-p org-projectile-file)
       (progn
-	(setq org-projectile-projects-file org-projectile-file)
-	(push (org-projectile-project-todo-entry :empty-lines 1)
+        (setq org-projectile-projects-file org-projectile-file)
+        (push (org-projectile-project-todo-entry :empty-lines 1)
               org-capture-templates))
     (org-projectile-per-project)
-    (setq org-projectile-per-project-filepath org-projectile-file)))
+    (setq org-projectile-per-project-filepath org-projectile-file))
+  )
+
+(use-package org-plus-contrib)
 
 (provide 'init-org)
 
