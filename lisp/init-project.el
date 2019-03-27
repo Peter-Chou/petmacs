2a90aaaa691e457c8afa16e0e4f513ab3dcfcfe2
Peter-Chou <zhou_00009@126.com>
2019-03-27 20:34:39 +0800

    speed up rg in project search

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -4,6 +4,10 @@
 
 ;;; Code:
 
+(eval-when-compile
+  (require 'init-const)
+  (require 'init-variable))
+
 (use-package projectile
   :diminish
   :bind (:map projectile-mode-map
@@ -17,22 +21,22 @@
   :config
   ;; (projectile-update-mode-line)         ; Update mode-line at the first time
 
-  ;; Use the faster searcher to handle project files:
-  ;; ripgrep `rg'
-  (let ((command
-         (when
-             (executable-find "rg")
-           (let ((rg-cmd ""))
-             (dolist (dir projectile-globally-ignored-directories)
-               (setq rg-cmd (format "%s --glob '!%s'" rg-cmd dir)))
-             (concat "rg -0 --files --color=never --hidden" rg-cmd)))
-         ))
-    (setq projectile-generic-command command))
-  (setq projectile-indexing-method 'alien)
-  (setq projectile-enable-caching nil)
-  ;; FIXME: too slow while getting submodule files on Windows
-  (setq projectile-git-submodule-command nil)
-  )
+  ;; Use the faster searcher to handle project files: ripgrep `rg'.
+  (when (executable-find "rg")
+    (setq projectile-generic-command
+          (let ((rg-cmd ""))
+            (dolist (dir projectile-globally-ignored-directories)
+              (setq rg-cmd (format "%s --glob '!%s'" rg-cmd dir)))
+            (concat "rg -0 --files --color=never --hidden" rg-cmd))))
+
+  ;; Faster searching on Windows
+  (when sys/win32p
+    (when (executable-find "rg")
+      (setq projectile-indexing-method 'alien)
+      (setq projectile-enable-caching nil))
+
+    ;; FIXME: too slow while getting submodule files on Windows
+    (setq projectile-git-submodule-command nil)))
 
 (provide 'init-project)
 
