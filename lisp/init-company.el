5f13d82e37d244ca28f030d4b8c653ffbef1648d
Peter-Chou <zhou_00009@126.com>
2019-04-12 21:47:05 +0800

    improve company-box

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -39,17 +39,68 @@
 	company-dabbrev-downcase nil)
   (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete))
 
+;; (when emacs/>=26p
+;;   (use-package company-box
+;;     :diminish
+;;     :functions (all-the-icons-faicon
+;;                 all-the-icons-material
+;;                 all-the-icons-octicon
+;;                 all-the-icons-alltheicon)
+;;     :hook (company-mode . company-box-mode)
+;;     :init (setq company-box-enable-icon (display-graphic-p))
+;;     :config
+;;     (setq company-box-backends-colors nil)))
 (when emacs/>=26p
   (use-package company-box
     :diminish
-    :functions (all-the-icons-faicon
-                all-the-icons-material
-                all-the-icons-octicon
-                all-the-icons-alltheicon)
     :hook (company-mode . company-box-mode)
-    :init (setq company-box-enable-icon (display-graphic-p))
+    :init (setq company-box-icons-alist 'company-box-icons-all-the-icons)
     :config
-    (setq company-box-backends-colors nil)))
+    (setq company-box-backends-colors nil)
+    (setq company-box-show-single-candidate t)
+    (setq company-box-max-candidates 50)
+
+    (defun company-box-icons--elisp (candidate)
+      (when (derived-mode-p 'emacs-lisp-mode)
+        (let ((sym (intern candidate)))
+          (cond ((fboundp sym) 'Function)
+                ((featurep sym) 'Module)
+                ((facep sym) 'Color)
+                ((boundp sym) 'Variable)
+                ((symbolp sym) 'Text)
+                (t . nil)))))
+
+    (with-eval-after-load 'all-the-icons
+      (declare-function all-the-icons-faicon 'all-the-icons)
+      (declare-function all-the-icons-material 'all-the-icons)
+      (setq company-box-icons-all-the-icons
+            `((Unknown . ,(all-the-icons-material "find_in_page" :height 0.9 :v-adjust -0.2))
+              (Text . ,(all-the-icons-faicon "text-width" :height 0.85 :v-adjust -0.05))
+              (Method . ,(all-the-icons-faicon "cube" :height 0.85 :v-adjust -0.05 :face 'all-the-icons-purple))
+              (Function . ,(all-the-icons-faicon "cube" :height 0.85 :v-adjust -0.05 :face 'all-the-icons-purple))
+              (Constructor . ,(all-the-icons-faicon "cube" :height 0.85 :v-adjust -0.05 :face 'all-the-icons-purple))
+              (Field . ,(all-the-icons-faicon "tag" :height 0.85 :v-adjust -0.05 :face 'all-the-icons-lblue))
+              (Variable . ,(all-the-icons-faicon "tag" :height 0.85 :v-adjust -0.05 :face 'all-the-icons-lblue))
+              (Class . ,(all-the-icons-material "settings_input_component" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-orange))
+              (Interface . ,(all-the-icons-material "share" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-lblue))
+              (Module . ,(all-the-icons-material "view_module" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-lblue))
+              (Property . ,(all-the-icons-faicon "wrench" :height 0.85 :v-adjust -0.05))
+              (Unit . ,(all-the-icons-material "settings_system_daydream" :height 0.9 :v-adjust -0.2))
+              (Value . ,(all-the-icons-material "format_align_right" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-lblue))
+              (Enum . ,(all-the-icons-material "storage" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-orange))
+              (Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.9 :v-adjust -0.2))
+              (Snippet . ,(all-the-icons-material "format_align_center" :height 0.9 :v-adjust -0.2))
+              (Color . ,(all-the-icons-material "palette" :height 0.9 :v-adjust -0.2))
+              (File . ,(all-the-icons-faicon "file-o" :height 0.9 :v-adjust -0.05))
+              (Reference . ,(all-the-icons-material "collections_bookmark" :height 0.9 :v-adjust -0.2))
+              (Folder . ,(all-the-icons-faicon "folder-open" :height 0.9 :v-adjust -0.05))
+              (EnumMember . ,(all-the-icons-material "format_align_right" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-lblue))
+              (Constant . ,(all-the-icons-faicon "square-o" :height 0.9 :v-adjust -0.05))
+              (Struct . ,(all-the-icons-material "settings_input_component" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-orange))
+              (Event . ,(all-the-icons-faicon "bolt" :height 0.85 :v-adjust -0.05 :face 'all-the-icons-orange))
+              (Operator . ,(all-the-icons-material "control_point" :height 0.9 :v-adjust -0.2))
+              (TypeParameter . ,(all-the-icons-faicon "arrows" :height 0.85 :v-adjust -0.05))
+              (Template . ,(all-the-icons-material "format_align_center" :height 0.9 :v-adjust -0.2)))))))
 
 (provide 'init-company)
 
