43d06de14652670b938734697a1a7f7dc38405c1
Peter-Chou <zhou_00009@126.com>
2019-04-15 14:31:18 +0800

    update ui setting

--- lisp/init-default.el
+++ lisp/init-default.el
@@ -95,13 +95,18 @@
 
 (setq-default major-mode 'text-mode)
 
-(if (fboundp 'scroll-bar-mode)
-    (scroll-bar-mode -1))
+;; UI
+(unless (eq window-system 'ns)
+  (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
 
 ;; Line and Column
 (setq-default fill-column 80)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
 (setq column-number-mode t)
 (setq line-number-mode t)
 
