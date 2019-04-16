b36efada54c400b48d10af17b223f249aea19542
Peter-Chou <zhou_00009@126.com>
2019-04-16 21:14:19 +0800

    improve ibuffer icons

--- lisp/init-ibuffer.el
+++ lisp/init-ibuffer.el
@@ -6,31 +6,40 @@
 
 (use-package ibuffer
   :ensure nil
-  :defines all-the-icons-icon-alist
   :functions (all-the-icons-icon-for-file
               all-the-icons-icon-for-mode
-              all-the-icons-match-to-alist
+              all-the-icons-auto-mode-match?
               all-the-icons-faicon)
   :commands ibuffer-find-file
   :bind ("C-x C-b" . ibuffer)
   :config
   (setq ibuffer-filter-group-name-face '(:inherit (font-lock-string-face bold)))
 
+  (add-hook 'ibuffer-mode-hook
+            (lambda ()
+              (setq tab-width 1)))
+
   ;; Display buffer icons on GUI
   (when (display-graphic-p)
-    (define-ibuffer-column icon (:name " ")
-      (let ((icon (if (and buffer-file-name
-                           (all-the-icons-match-to-alist buffer-file-name
-                                                         all-the-icons-icon-alist))
-                      (all-the-icons-icon-for-file (file-name-nondirectory buffer-file-name)
+    ;; To be correctly aligned, the size of the name field must be equal to that
+    ;; of the icon column below, plus 1 (for the tab I inserted)
+    (define-ibuffer-column icon (:name "   ")
+      (let ((icon (if (and (buffer-file-name)
+                           (all-the-icons-auto-mode-match?))
+                      (all-the-icons-icon-for-file (file-name-nondirectory (buffer-file-name))
                                                    :height 0.9 :v-adjust -0.05)
                     (all-the-icons-icon-for-mode major-mode :height 0.9 :v-adjust -0.05))))
         (if (symbolp icon)
-            (setq icon (all-the-icons-faicon "file-o" :face 'all-the-icons-dsilver :height 0.9 :v-adjust -0.05))
+            (setq icon (all-the-icons-faicon "file-o"
+                                             :face 'all-the-icons-dsilver
+                                             :height 0.9
+                                             :v-adjust -0.05))
           icon)))
 
     (setq ibuffer-formats '((mark modified read-only locked
-                                  " " (icon 2 2 :left :elide) (name 18 18 :left :elide)
+                                  ;; Here you may adjust by replacing :right with :center or :left
+                                  ;; According to taste, if you want the icon further from the name
+                                  " " (icon 2 2 :right :elide) "\t" (name 18 18 :left :elide)
                                   " " (size 9 -1 :right)
                                   " " (mode 16 16 :left :elide) " " filename-and-process)
                             (mark " " (name 16 -1) " " filename))))
@@ -55,7 +64,6 @@
                                       :height 1.25)
                " ")
             "Project: "))))
-
 
 (provide 'init-ibuffer)
 
