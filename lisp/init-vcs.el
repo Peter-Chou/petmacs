c9761320245d31eaa547bfde47346eff1ebcedac
Peter-Chou <zhou_00009@126.com>
2019-05-07 13:57:53 +0800

    refactor

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -39,39 +39,35 @@
   (define-key magit-mode-map (kbd "M-5") 'winum-select-window-5)
   (define-key magit-mode-map (kbd "M-6") 'winum-select-window-6)
   (define-key magit-mode-map (kbd "M-7") 'winum-select-window-7)
-  (define-key magit-mode-map (kbd "M-8") 'winum-select-window-8)
+  (define-key magit-mode-map (kbd "M-8") 'winum-select-window-8))
 
-  ;; Show tasks
-  (use-package magit-todos
-    :hook (ater-init . magit-todos-mode))
+;; Walk through git revisions of a file
+(use-package git-timemachine
+  :custom-face
+  (git-timemachine-minibuffer-author-face ((t (:inherit font-lock-string-face))))
+  (git-timemachine-minibuffer-detail-face ((t (:inherit warning))))
+  :bind (:map vc-prefix-map
+		("t" . git-timemachine)))
 
-  ;; Walk through git revisions of a file
-  (use-package git-timemachine
-    :custom-face
-    (git-timemachine-minibuffer-author-face ((t (:inherit font-lock-string-face))))
-    (git-timemachine-minibuffer-detail-face ((t (:inherit warning))))
-    :bind (:map vc-prefix-map
-		  ("t" . git-timemachine)))
+;; Pop up last commit information of current line
+(use-package git-messenger
+  :bind (:map vc-prefix-map
+		("p" . git-messenger:popup-message)
+		:map git-messenger-map
+		("m" . git-messenger:copy-message))
+  :init
+  ;; Use magit-show-commit for showing status/diff commands
+  (setq git-messenger:use-magit-popup t))
 
-  ;; Pop up last commit information of current line
-  (use-package git-messenger
-    :bind (:map vc-prefix-map
-		  ("p" . git-messenger:popup-message)
-		  :map git-messenger-map
-		  ("m" . git-messenger:copy-message))
-    :init
-    ;; Use magit-show-commit for showing status/diff commands
-    (setq git-messenger:use-magit-popup t))
-
-  ;; Resolve diff3 conflicts
-  (use-package smerge-mode
-    :ensure nil
-    :diminish
-    :preface
-    (with-eval-after-load 'hydra
-      (defhydra smerge-hydra
-	(:color pink :hint nil :post (smerge-auto-leave))
-	"
+;; Resolve diff3 conflicts
+(use-package smerge-mode
+  :ensure nil
+  :diminish
+  :preface
+  (with-eval-after-load 'hydra
+    (defhydra smerge-hydra
+      (:color pink :hint nil :post (smerge-auto-leave))
+      "
 ^Move^       ^Keep^               ^Diff^                 ^Other^
 ^^-----------^^-------------------^^---------------------^^-------
 _n_ext       _b_ase               _<_: upper/base        _C_ombine
@@ -80,41 +76,41 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
 ^^           _a_ll                _R_efine
 ^^           _RET_: current       _E_diff
 "
-	("n" smerge-next)
-	("p" smerge-prev)
-	("b" smerge-keep-base)
-	("u" smerge-keep-upper)
-	("l" smerge-keep-lower)
-	("a" smerge-keep-all)
-	("RET" smerge-keep-current)
-	("\C-m" smerge-keep-current)
-	("<" smerge-diff-base-upper)
-	("=" smerge-diff-upper-lower)
-	(">" smerge-diff-base-lower)
-	("R" smerge-refine)
-	("E" smerge-ediff)
-	("C" smerge-combine-with-next)
-	("r" smerge-resolve)
-	("k" smerge-kill-current)
-	("ZZ" (lambda ()
-		(interactive)
-		(save-buffer)
-		(bury-buffer))
-	 "Save and bury buffer" :color blue)
-	("q" nil "cancel" :color blue)))
-    :hook ((find-file . (lambda ()
-                          (save-excursion
-                            (goto-char (point-min))
-                            (when (re-search-forward "^<<<<<<< " nil t)
-                              (smerge-mode 1)))))
-           (magit-diff-visit-file . (lambda ()
-                                      (when smerge-mode
-					(smerge-hydra/body))))))
+      ("n" smerge-next)
+      ("p" smerge-prev)
+      ("b" smerge-keep-base)
+      ("u" smerge-keep-upper)
+      ("l" smerge-keep-lower)
+      ("a" smerge-keep-all)
+      ("RET" smerge-keep-current)
+      ("\C-m" smerge-keep-current)
+      ("<" smerge-diff-base-upper)
+      ("=" smerge-diff-upper-lower)
+      (">" smerge-diff-base-lower)
+      ("R" smerge-refine)
+      ("E" smerge-ediff)
+      ("C" smerge-combine-with-next)
+      ("r" smerge-resolve)
+      ("k" smerge-kill-current)
+      ("ZZ" (lambda ()
+	      (interactive)
+	      (save-buffer)
+	      (bury-buffer))
+       "Save and bury buffer" :color blue)
+      ("q" nil "cancel" :color blue)))
+  :hook ((find-file . (lambda ()
+                         (save-excursion
+                           (goto-char (point-min))
+                           (when (re-search-forward "^<<<<<<< " nil t)
+                             (smerge-mode 1)))))
+          (magit-diff-visit-file . (lambda ()
+                                     (when smerge-mode
+				       (smerge-hydra/body))))))
 
-  ;; Open github/gitlab/bitbucket page
-  (use-package browse-at-remote
-    :bind (:map vc-prefix-map
-		("B" . browse-at-remote))))
+;; Open github/gitlab/bitbucket page
+(use-package browse-at-remote
+  :bind (:map vc-prefix-map
+		("B" . browse-at-remote)))
 
 (use-package git-gutter
   :hook (prog-mode . git-gutter-mode)
@@ -136,8 +132,7 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
         git-gutter:ask-p nil
         git-gutter:verbosity 0
         git-gutter:handled-backends '(git hg bzr svn)
-        git-gutter:hide-gutter t)
-  )
+        git-gutter:hide-gutter t))
 
 (use-package evil-magit
   ;; :pin melpa-stable
@@ -145,6 +140,16 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
   (with-eval-after-load 'magit
     (require 'evil-magit)
     (add-hook 'git-commit-mode-hook 'evil-insert-state)))
+
+;; Show tasks
+(use-package magit-todos
+  :ensure t
+  :commands (magit-todos-mode magit-todos-list magit-todos-list-internal)
+  :quelpa (magit-todos :repo "Peter-Chou/magit-todos" :fetcher github)
+  :hook (after-init . magit-todos-mode)
+  :config
+  (setq magit-todos-nice nil)
+  (setq magit-todos-scanner 'magit-todos--scan-with-git-grep))
 
 ;; Git related modes
 (use-package gitattributes-mode)
