adf942d36d198479af4acf66b43380be96d146e6
Peter-Chou <zhou_00009@126.com>
2019-05-07 09:34:40 +0800

    :lipstick:

--- custom.el
+++ custom.el
@@ -3,12 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(aw-keys (quote (106 107 108 105 111 104 121 117 112)) t)
+ '(beacon-color "yellow")
  '(doom-themes-enable-bold t)
  '(doom-themes-enable-italic t)
- '(org-agenda-files (quote ("c:/projects/Eastday.Classifier/TODOs.org" "c:/projects/Eastday.Seg/TODOs.org")))
- '(package-selected-packages
-   (quote
-    (ein py-isort focus evil-org org-pomodoro ob-ipython org-dashboard org-preview-html org-projectile org-plus-contrib org-bullets yasnippet-snippets yapfify yaml-mode winum which-key visual-fill-column virtualenvwrapper vimish-fold treemacs-projectile treemacs-evil toc-org symbol-overlay sqlup-mode sql-indent smart-semicolon rg restart-emacs ranger rainbow-delimiters quelpa-use-package pyvenv prettify-utils prettier-js posframe popwin persp-mode-projectile-bridge paradox olivetti magit-todos lsp-ui json-mode ivy-yasnippet ivy-xref ivy-hydra imenu-list ibuffer-projectile hungry-delete hlinum highlight-indent-guides hide-mode-line gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-gutter fontify-face font-lock+ flycheck flx fill-column-indicator expand-region evil-visualstar evil-surround evil-major-leader evil-fringe-mark evil-escape evil-commentary evil-anzu esup eshell-z eshell-prompt-extras esh-help elisp-refs electric-operator eldoc-box editorconfig doom-themes doom-modeline diredfl dired-quick-sort diminish diffview default-text-scale dashboard daemons counsel-tramp counsel-projectile company-lsp company-box company-anaconda centered-cursor-mode ccls carbon-now-sh browse-at-remote beacon amx all-the-icons-ivy all-the-icons-dired alert aggressive-indent))))
+ '(git-gutter:added-sign "+")
+ '(git-gutter:deleted-sign "-")
+ '(git-gutter:modified-sign " ")
+ '(org-agenda-files nil))
  
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -16,6 +18,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(all-the-icons-dired-dir-face ((t (\` (:foreground (\, (face-background (quote default))))))))
+ '(all-the-icons-dsilver ((((background dark)) :foreground "#838484") (((background light)) :foreground "#838484")))
+ '(all-the-icons-lsilver ((((background dark)) :foreground "#B9B6AA") (((background light)) :foreground "#7F7869")))
+ '(all-the-icons-silver ((((background dark)) :foreground "#716E68") (((background light)) :foreground "#716E68")))
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
  '(doom-modeline-buffer-file ((t (:inherit font-lock-string-face :weight bold))))
  '(epe-pipeline-delimiter-face ((t (:foreground "#fd780f" :weight bold))))
