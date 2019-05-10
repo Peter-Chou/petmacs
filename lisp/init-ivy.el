ed33a7a872f5298c24967690a1f0fa0e9ad4617a
Peter-Chou <zhou_00009@126.com>
2019-05-10 17:30:22 +0800

    improve ivy-fly

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -77,7 +77,7 @@
           :map swiper-map
           ("M-%" . swiper-query-replace))
   :hook ((after-init . ivy-mode)
-         (ivy-mode . counsel-mode))
+          (ivy-mode . counsel-mode))
   :config
   (setq enable-recursive-minibuffers t) ; Allow commands in minibuffers
 
@@ -153,7 +153,9 @@
                  (append unread-command-events
                          (listify-key-sequence (kbd "M-p")))))
           ((memq this-command '(self-insert-command
-                                ivy-yank-word))
+				yank
+                                ivy-yank-word
+                                counsel-yank-pop))
            (delete-region (point)
                           (point-max)))))
 
