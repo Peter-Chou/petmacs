42b0c7da4e97250b9556eb75d4a8d1c8bc277d49
Peter-Chou <zhou_00009@126.com>
2019-05-13 21:56:23 +0800

    speedup startup

--- init.el
+++ init.el
@@ -9,24 +9,33 @@
 
 ;; Speed up startup
 (defvar default-file-name-handler-alist file-name-handler-alist)
-
-(setq package-enable-at-startup nil
-      file-name-handler-alist nil
-      message-log-max 16384
-      gc-cons-threshold 40000000
-      )
-
+(setq file-name-handler-alist nil)
+(setq gc-cons-threshold 40000000)
 (add-hook 'emacs-startup-hook
           (lambda ()
-            "Restore defalut values after init."
+            "Restore defalut values after startup."
             (setq file-name-handler-alist default-file-name-handler-alist)
             (setq gc-cons-threshold 800000)
+
+            ;; GC automatically while unfocusing the frame
+            ;; `focus-out-hook' is obsolete since 27.1
             (if (boundp 'after-focus-change-function)
                 (add-function :after after-focus-change-function
                               (lambda ()
                                 (unless (frame-focus-state)
                                   (garbage-collect))))
-              (add-hook 'focus-out-hook 'garbage-collect))))
+              (add-hook 'focus-out-hook 'garbage-collect))
+
+            ;; Avoid GCs while using `ivy'/`counsel'/`swiper' and `helm', etc.
+            ;; @see http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/
+            (defun my-minibuffer-setup-hook ()
+              (setq gc-cons-threshold 20000000))
+
+            (defun my-minibuffer-exit-hook ()
+              (setq gc-cons-threshold 800000))
+
+            (add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
+            (add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook)))
 
 ;; Load path
 ;; Optimize: Force "lisp"" and "site-lisp" at the head to reduce the startup time.
