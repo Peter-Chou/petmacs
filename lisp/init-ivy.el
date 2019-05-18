2a80572be69503b346b9258622954151780750ab
Peter-Chou <zhou_00009@126.com>
2019-05-18 14:08:31 +0800

    improve amx package

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -9,75 +9,75 @@
   :defines (projectile-completion-system magit-completing-read-function)
   :commands swiper-isearch
   :bind (("C-s" . swiper-isearch)
-          ("s-f" . swiper)
-          ("C-S-s" . swiper-all)
+         ("s-f" . swiper)
+         ("C-S-s" . swiper-all)
 
-          ("C-c C-r" . ivy-resume)
-          ("C-c v p" . ivy-push-view)
-          ("C-c v o" . ivy-pop-view)
-          ("C-c v ." . ivy-switch-view)
+         ("C-c C-r" . ivy-resume)
+         ("C-c v p" . ivy-push-view)
+         ("C-c v o" . ivy-pop-view)
+         ("C-c v ." . ivy-switch-view)
 
-          :map counsel-mode-map
-          ([remap swiper] . counsel-grep-or-swiper)
-          ([remap dired] . counsel-dired)
-          ("C-x C-r" . counsel-recentf)
-          ("C-x j" . counsel-mark-ring)
+         :map counsel-mode-map
+         ([remap swiper] . counsel-grep-or-swiper)
+         ([remap dired] . counsel-dired)
+         ("C-x C-r" . counsel-recentf)
+         ("C-x j" . counsel-mark-ring)
 
-          ("C-c L" . counsel-load-library)
-          ("C-c P" . counsel-package)
-          ("C-c f" . counsel-find-library)
-          ("C-c g" . counsel-grep)
-          ("C-c h" . counsel-command-history)
-          ("C-c i" . counsel-git)
-          ("C-c j" . counsel-git-grep)
-          ("C-c l" . counsel-locate)
-          ("C-c r" . counsel-rg)
-          ("C-c z" . counsel-fzf)
+         ("C-c L" . counsel-load-library)
+         ("C-c P" . counsel-package)
+         ("C-c f" . counsel-find-library)
+         ("C-c g" . counsel-grep)
+         ("C-c h" . counsel-command-history)
+         ("C-c i" . counsel-git)
+         ("C-c j" . counsel-git-grep)
+         ("C-c l" . counsel-locate)
+         ("C-c r" . counsel-rg)
+         ("C-c z" . counsel-fzf)
 
-          ("C-c c L" . counsel-load-library)
-          ("C-c c P" . counsel-package)
-          ("C-c c a" . counsel-apropos)
-          ("C-c c e" . counsel-colors-emacs)
-          ("C-c c f" . counsel-find-library)
-          ("C-c c g" . counsel-grep)
-          ("C-c c h" . counsel-command-history)
-          ("C-c c i" . counsel-git)
-          ("C-c c j" . counsel-git-grep)
-          ("C-c c l" . counsel-locate)
-          ("C-c c m" . counsel-minibuffer-history)
-          ("C-c c o" . counsel-outline)
-          ("C-c c p" . counsel-pt)
-          ("C-c c r" . counsel-rg)
-          ("C-c c s" . counsel-ag)
-          ("C-c c t" . counsel-load-theme)
-          ("C-c c u" . counsel-unicode-char)
-          ("C-c c w" . counsel-colors-web)
-          ("C-c c z" . counsel-fzf)
+         ("C-c c L" . counsel-load-library)
+         ("C-c c P" . counsel-package)
+         ("C-c c a" . counsel-apropos)
+         ("C-c c e" . counsel-colors-emacs)
+         ("C-c c f" . counsel-find-library)
+         ("C-c c g" . counsel-grep)
+         ("C-c c h" . counsel-command-history)
+         ("C-c c i" . counsel-git)
+         ("C-c c j" . counsel-git-grep)
+         ("C-c c l" . counsel-locate)
+         ("C-c c m" . counsel-minibuffer-history)
+         ("C-c c o" . counsel-outline)
+         ("C-c c p" . counsel-pt)
+         ("C-c c r" . counsel-rg)
+         ("C-c c s" . counsel-ag)
+         ("C-c c t" . counsel-load-theme)
+         ("C-c c u" . counsel-unicode-char)
+         ("C-c c w" . counsel-colors-web)
+         ("C-c c z" . counsel-fzf)
 
-          ;; Find counsel commands quickly
-          ("<f6>" . (lambda ()
-                      (interactive)
-                      (counsel-M-x "^counsel ")))
+         ;; Find counsel commands quickly
+         ("<f6>" . (lambda ()
+                     (interactive)
+                     (counsel-M-x "^counsel ")))
 
-          :map ivy-minibuffer-map
-          ("C-w" . ivy-yank-word)
+         :map ivy-minibuffer-map
+         ("C-w" . ivy-yank-word)
 
-          ;; Search at point
-          ;; "M-j": word-at-point
-          ;; "M-n"/"C-w": symbol-at-point
-          ;; Refer to https://www.emacswiki.org/emacs/SearchAtPoint#toc8
-          ;; and https://github.com/abo-abo/swiper/wiki/FAQ
-          ;; ("C-w" . (lambda ()
-          ;;            (interactive)
-          ;;            (insert (format "%s" (with-ivy-window (ivy-thing-at-point))))))
+         ;; Search at point
+         ;; "M-j": word-at-point
+         ;; "M-n"/"C-w": symbol-at-point
+         ;; Refer to https://www.emacswiki.org/emacs/SearchAtPoint#toc8
+         ;; and https://github.com/abo-abo/swiper/wiki/FAQ
+         ;; ("C-w" . (lambda ()
+         ;;            (interactive)
+         ;;            (insert (format "%s" (with-ivy-window (ivy-thing-at-point))))))
 
-          :map counsel-find-file-map
-          ("C-h" . counsel-up-directory)
+         :map counsel-find-file-map
+         ("C-h" . counsel-up-directory)
 
-          :map swiper-map
-          ("M-%" . swiper-query-replace))
+         :map swiper-map
+         ("M-%" . swiper-query-replace))
   :hook ((after-init . ivy-mode)
-          (ivy-mode . counsel-mode))
+         (ivy-mode . counsel-mode))
   :config
   (setq enable-recursive-minibuffers t) ; Allow commands in minibuffers
 
@@ -207,7 +207,8 @@
   (use-package flx)
 
   ;; Enhance M-x
-  (use-package amx)
+  (use-package amx
+    :init (setq amx-history-length 20))
 
   ;; Additional key bindings for Ivy
   (use-package ivy-hydra
