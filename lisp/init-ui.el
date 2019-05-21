d457692219b89ed825bb633466fd0803fd32b392
Peter-Chou <zhou_00009@126.com>
2019-05-21 21:55:48 +0800

    improve all-the-icons

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -12,18 +12,19 @@
 ;; NOTE: Must run `M-x all-the-icons-install-fonts' manually on Windows
 (use-package all-the-icons
   :if (display-graphic-p)
-  :custom-face
-  ;; Reset colors since they are too dark in `doom-themes'
-  (all-the-icons-silver ((((background dark)) :foreground "#716E68")
-                         (((background light)) :foreground "#716E68")))
-  (all-the-icons-lsilver ((((background dark)) :foreground "#B9B6AA")
-                          (((background light)) :foreground "#7F7869")))
-  (all-the-icons-dsilver ((((background dark)) :foreground "#838484")
-                          (((background light)) :foreground "#838484")))
-  :init
-  (unless (or sys/win32p (member "all-the-icons" (font-family-list)))
-    (all-the-icons-install-fonts t))
+  :init (unless (or sys/win32p (member "all-the-icons" (font-family-list)))
+          (all-the-icons-install-fonts t))
   :config
+  (add-to-list 'all-the-icons-icon-alist
+               '("\\.xpm$" all-the-icons-octicon "file-media" :v-adjust 0.0 :face all-the-icons-dgreen))
+  (add-to-list 'all-the-icons-icon-alist
+               '("\\.lua$" all-the-icons-fileicon "lua" :face all-the-icons-dblue))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(lua-mode all-the-icons-fileicon "lua" :face all-the-icons-dblue))
+  (add-to-list 'all-the-icons-icon-alist
+               '("\\.go$" all-the-icons-fileicon "go" :face all-the-icons-blue))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(go-mode all-the-icons-fileicon "go" :face all-the-icons-blue))
   (add-to-list 'all-the-icons-mode-icon-alist
                '(help-mode all-the-icons-faicon "info-circle" :height 1.1 :v-adjust -0.1 :face all-the-icons-purple))
   (add-to-list 'all-the-icons-mode-icon-alist
@@ -31,9 +32,19 @@
   (add-to-list 'all-the-icons-icon-alist
                '("NEWS$" all-the-icons-faicon "newspaper-o" :height 0.9 :v-adjust -0.2))
   (add-to-list 'all-the-icons-icon-alist
-               '("Cask\\'" all-the-icons-fileicon "elisp" :height 1.0 :face all-the-icons-blue))
+               '("\\.el$" all-the-icons-fileicon "elisp" :height 0.9 :v-adjust -0.1 :face all-the-icons-purple))
   (add-to-list 'all-the-icons-mode-icon-alist
-               '(cask-mode all-the-icons-fileicon "elisp" :height 1.0 :face all-the-icons-blue))
+               '(emacs-lisp-mode all-the-icons-fileicon "elisp" :height 0.9 :v-adjust -0.1 :face all-the-icons-purple))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(inferior-emacs-lisp-mode  all-the-icons-fileicon "elisp" :height 0.9 :v-adjust -0.1 :face all-the-icons-lblue))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(fundamental-mode all-the-icons-fileicon "elisp" :height 0.9 :v-adjust -0.1 :face all-the-icons-dsilver))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(special-mode all-the-icons-fileicon "elisp" :height 0.9 :v-adjust -0.1 :face all-the-icons-yellow))
+  (add-to-list 'all-the-icons-icon-alist
+               '("Cask\\'" all-the-icons-fileicon "elisp" :height 0.9 :v-adjust -0.1 :face all-the-icons-blue))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(cask-mode all-the-icons-fileicon "elisp" :height 0.9 :v-adjust -0.1 :face all-the-icons-blue))
   (add-to-list 'all-the-icons-icon-alist
                '(".*\\.ipynb\\'" all-the-icons-fileicon "jupyter" :height 1.2 :face all-the-icons-orange))
   (add-to-list 'all-the-icons-mode-icon-alist
@@ -43,9 +54,9 @@
   (add-to-list 'all-the-icons-mode-icon-alist
                '(ein:notebook-multilang-mode all-the-icons-fileicon "jupyter" :height 1.2 :face all-the-icons-orange))
   (add-to-list 'all-the-icons-icon-alist
-               '("\\.epub$" all-the-icons-faicon "book" :face all-the-icons-green))
+               '("\\.epub\\'" all-the-icons-faicon "book" :height 1.0 :v-adjust -0.1 :face all-the-icons-green))
   (add-to-list 'all-the-icons-mode-icon-alist
-               '(nov-mode all-the-icons-faicon "book" :face all-the-icons-green))
+               '(nov-mode all-the-icons-faicon "book" :height 1.0 :v-adjust -0.1 :face all-the-icons-green))
   (add-to-list 'all-the-icons-mode-icon-alist
                '(gfm-mode  all-the-icons-octicon "markdown" :face all-the-icons-blue)))
 
