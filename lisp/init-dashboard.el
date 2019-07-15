af3140c49ae1648fa4a68c4842ea11480bcf61cf
Peter-Chou <zhou_00009@126.com>
2019-07-15 22:35:03 +0800

    improve dashboard ui

--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -4,51 +4,50 @@
 
 ;;; Code:
 
+(use-package page-break-lines
+  :commands (page-break-lines-mode))
+
 (use-package dashboard
+  :custom-face (dashboard-banner-logo-title ((t (:height 1.1 :inherit default))))
   :diminish (dashboard-mode page-break-lines-mode)
   :defines (persp-save-dir persp-special-last-buffer)
   :functions (all-the-icons-faicon
-              all-the-icons-material
-              open-custom-file
-              persp-get-buffer-or-null
-              persp-load-state-from-file
-              persp-switch-to-buffer
-              winner-undo
-              widget-forward)
-  :hook (
-	 (after-init . dashboard-setup-startup-hook)
-	 (dashboard-mode  . (lambda ()
-			      (display-line-numbers-mode -1)
-			      (hl-line-mode -1)
-			      (setq-local frame-title-format "")
-			      (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*"))))))
-  :config
-  (setq dashboard-banner-logo-title "Petmacs --- Adorable just like A PET")
-  (setq dashboard-center-content t)
-  ;; (setq dashboard-set-heading-icons t)
-  ;; dashboard-heading-icons '((recents   . "file-text")
-  ;;                           (bookmarks . "bookmark")
-  ;;                           (agenda    . "calendar")
-  ;;                           (projects  . "file-directory")
-  ;;                           (registers . "database"))
-  (setq dashboard-footer "  S T A Y   H U N G R Y,   S T A Y   F O O L I S H.")
-  (setq dashboard-footer-icon (cond ((display-graphic-p)
-				     (all-the-icons-faicon "heart"
-							   :height 1.1
-							   :v-adjust -0.05
-							   :face 'error))
-				    ((char-displayable-p ?🧡) "🧡 ")
-				    (t ">")))
-  (setq dashboard-set-file-icons t)
-  (setq dashboard-show-shortcuts nil)
-  (setq dashboard-set-init-info t)
-  (setq show-week-agenda-p t)
-  (setq dashboard-startup-banner (expand-file-name "img/totoro_banner.png" user-emacs-directory))
-  (setq dashboard-items '((recents  . 7)
-                          (bookmarks . 5)
-                          ;; (projects . 5)
-                          (agenda . 5)))
+	      all-the-icons-material
+	      open-custom-file
+	      persp-get-buffer-or-null
+	      persp-load-state-from-file
+	      persp-switch-to-buffer
+	      winner-undo
+	      widget-forward)
+  :hook ((after-init . dashboard-setup-startup-hook)
+  	 (dashboard-mode  . (lambda ()
+  			      (display-line-numbers-mode -1)
+  			      (hl-line-mode -1)
+  			      (setq-local frame-title-format "")
+  			      (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*"))))))
+  :init
+  (setq
+   dashboard-banner-logo-title "Petmacs --- Adorable just like A PET"
+   dashboard-startup-banner (expand-file-name "img/totoro_banner.png" user-emacs-directory)
+   dashboard-center-content t
+   dashboard-show-shortcuts nil
+   dashboard-items '((recents  . 7)
+		     (bookmarks . 5)
+		     ;; (projects . 5)
+		     (agenda . 5))
+   dashboard-set-init-info t
+   dashboard-set-file-icons t
+   dashboard-set-heading-icons t
+   dashboard-heading-icons '((recents   . "file-text")
+			     (bookmarks . "bookmark")
+			     (agenda    . "calendar")
+			     (projects  . "file-directory")
+			     (registers . "database"))
+   dashboard-set-footer t
+   dashboard-footer "  S T A Y   H U N G R Y,   S T A Y   F O O L I S H"
+   )
 
+  :config
   (evil-define-key 'normal dashboard-mode-map
     (kbd "RET") 'widget-button-press
     (kbd "gd") 'widget-button-press
@@ -61,10 +60,9 @@
     (kbd "}") #'dashboard-next-section
     (kbd "{") #'dashboard-previous-section
     )
-  )
-  ;; (evil-define-key 'normal dashboard-mode-map (kbd "gd") 'widget-button-press)
-  ;; (evil-define-key 'normal dashboard-mode-map [mouse-1] 'widget-button-click))
+  (evil-define-key 'normal dashboard-mode-map (kbd "gd") 'widget-button-press)
+  (evil-define-key 'normal dashboard-mode-map [mouse-1] 'widget-button-click))
 
-  (provide 'init-dashboard)
+(provide 'init-dashboard)
 
 ;;; init-dashboard.el ends here
