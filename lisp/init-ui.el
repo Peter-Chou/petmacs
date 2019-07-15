ef16ff0de94afaf879e082f1d4f36b0e5f99f00f
Peter-Chou <zhou_00009@126.com>
2019-07-15 22:34:33 +0800

    refactor doom-modeline config

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -60,37 +60,35 @@
   (unless after-init-time
     ;; prevent flash of unstyled modeline at startup
     (setq-default mode-line-format nil))
+  (setq
+   find-file-visit-truename t  ; display the real names for symlink files
+   ;; doom-modeline-height 21
+   doom-modeline-lsp nil
+   doom-modeline-persp-name nil
+   doom-modeline-github nil
+   ;; doom-modeline-buffer-file-name-style 'truncate-with-project
+   doom-modeline-buffer-file-name-style 'file-name
+   doom-modeline-major-mode-color-icon t)
   :config
-  (progn
-    (setq
-     find-file-visit-truename t  ; display the real names for symlink files
-     ;; doom-modeline-height 21
-     doom-modeline-lsp nil
-     doom-modeline-persp-name nil
-     doom-modeline-github nil
-     ;; doom-modeline-buffer-file-name-style 'truncate-with-project
-     doom-modeline-buffer-file-name-style 'file-name
-     doom-modeline-major-mode-color-icon t)
+  (doom-modeline-def-segment my-python-venv
+    "The current python virtual environment state."
+    (when (eq major-mode 'python-mode)
+      (if (eq python-shell-virtualenv-root nil)
+	  ""
+	(propertize
+	 (let ((base-dir-name (file-name-nondirectory (substring python-shell-virtualenv-root 0 -1))))
+	   (if (< 10 (length base-dir-name))
+	       (format " (%s..)" (substring base-dir-name 0 8))
+	     (format " (%s)" base-dir-name)))
+	 'face (if (doom-modeline--active) 'doom-modeline-buffer-major-mode)))))
 
-    (doom-modeline-def-segment my-python-venv
-      "The current python virtual environment state."
-      (when (eq major-mode 'python-mode)
-        (if (eq python-shell-virtualenv-root nil)
-            ""
-          (propertize
-           (let ((base-dir-name (file-name-nondirectory (substring python-shell-virtualenv-root 0 -1))))
-             (if (< 10 (length base-dir-name))
-                 (format " (%s..)" (substring base-dir-name 0 8))
-               (format " (%s)" base-dir-name)))
-           'face (if (doom-modeline--active) 'doom-modeline-buffer-major-mode)))))
-
-    (doom-modeline-def-modeline 'my-modeline-layout
-      '(bar workspace-name window-number modals matches buffer-info remote-host buffer-position parrot selection-info)
-      '(objed-state misc-info persp-name lsp irc mu4e github debug fancy-battery minor-modes input-method buffer-encoding my-python-venv process vcs checker))
+  (doom-modeline-def-modeline 'my-modeline-layout
+    '(bar workspace-name window-number modals matches buffer-info remote-host buffer-position parrot selection-info)
+    '(objed-state misc-info persp-name lsp irc mu4e github debug fancy-battery minor-modes input-method buffer-encoding my-python-venv process vcs checker))
 
 
-    (defun setup-custom-doom-modeline ()
-      (doom-modeline-set-modeline 'my-modeline-layout 'default))))
+  (defun setup-custom-doom-modeline ()
+    (doom-modeline-set-modeline 'my-modeline-layout 'default)))
 
 (use-package doom-themes
   :defer nil
