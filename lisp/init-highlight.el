bdb221a00b9aef1ae9c0e6677ee225fc0933063c
Peter-Chou <zhou_00009@126.com>
2019-08-14 09:20:23 +0800

    improve highlight-indent-guides config

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -55,49 +55,55 @@
   (highlight-indent-guides-top-character-face ((t (:inherit (font-lock-keyword-face bold)))))
   (highlight-indent-guides-character-face ((t (:inherit (font-lock-comment-face)))))
   :commands highlight-indent-guides--highlighter-default
-  :functions my-indent-guides-for-all-but-first-column
-  :defer t
-  ;; :hook ((python-mode yaml-mode) . highlight-indent-guides-mode)
+  :functions (ivy-cleanup-string
+	      my-ivy-cleanup-indentation
+	      my-indent-guides-for-all-but-first-column)
   :hook (prog-mode . highlight-indent-guides-mode)
-  :config
+  :init
   (setq highlight-indent-guides-method 'character
 	highlight-indent-guides-character ?\┆ ;; candidates: , ⋮, ┆, ┊, ┋, ┇
 	highlight-indent-guides-responsive 'top
 	highlight-indent-guides-auto-enabled nil
 	highlight-indent-guides-auto-character-face-perc 10
 	highlight-indent-guides-auto-top-character-face-perc 20)
-  ;; Don't display indentations while editing with `company'
-  (with-eval-after-load 'company
-    (add-hook 'company-completion-started-hook
-              (lambda (&rest _)
-                "Trun off indentation highlighting."
-                (when highlight-indent-guides-mode
-                  (highlight-indent-guides-mode -1))))
-    (add-hook 'company-after-completion-hook
-              (lambda (&rest _)
-                "Trun on indentation highlighting."
-                (when (and (derived-mode-p 'prog-mode)
-                           (not highlight-indent-guides-mode))
-                  (highlight-indent-guides-mode 1)))))
+    :config
+    ;; Don't display indentations while editing with `company'
+    (with-eval-after-load 'company
+      (add-hook 'company-completion-started-hook
+                (lambda (&rest _)
+                  "Trun off indentation highlighting."
+                  (when highlight-indent-guides-mode
+                    (highlight-indent-guides-mode -1))))
+      (add-hook 'company-after-completion-hook
+                (lambda (&rest _)
+                  "Trun on indentation highlighting."
+                  (when (and (derived-mode-p 'prog-mode)
+                             (not highlight-indent-guides-mode))
+                    (highlight-indent-guides-mode 1)))))
 
-  ;; Don't display first level of indentation
-  (defun my-indent-guides-for-all-but-first-column (level responsive display)
-    (unless (< level 1)
-      (highlight-indent-guides--highlighter-default level responsive display)))
-  (setq highlight-indent-guides-highlighter-function
-        #'my-indent-guides-for-all-but-first-column)
+    ;; Don't display first level of indentation
+    (defun my-indent-guides-for-all-but-first-column (level responsive display)
+      (unless (< level 1)
+        (highlight-indent-guides--highlighter-default level responsive display)))
+    (setq highlight-indent-guides-highlighter-function
+          #'my-indent-guides-for-all-but-first-column)
 
-  ;; Don't display indentations in `swiper'
-  ;; https://github.com/DarthFennec/highlight-indent-guides/issues/40
-  (with-eval-after-load 'ivy
-    (defadvice ivy-cleanup-string (after my-ivy-cleanup-hig activate)
-      (let ((pos 0) (next 0) (limit (length str)) (prop 'highlight-indent-guides-prop))
-        (while (and pos next)
-          (setq next (text-property-not-all pos limit prop nil str))
-          (when next
-            (setq pos (text-property-any next limit prop nil str))
-            (ignore-errors
-              (remove-text-properties next pos '(display nil face nil) str))))))))
+    ;; Don't display indentations in `swiper'
+    ;; https://github.com/DarthFennec/highlight-indent-guides/issues/40
+    (with-eval-after-load 'ivy
+      (defun my-ivy-cleanup-indentation (str)
+        "Clean up indentation highlighting in ivy minibuffer."
+        (let ((pos 0)
+              (next 0)
+              (limit (length str))
+              (prop 'highlight-indent-guides-prop))
+          (while (and pos next)
+            (setq next (text-property-not-all pos limit prop nil str))
+            (when next
+              (setq pos (text-property-any next limit prop nil str))
+              (ignore-errors
+                (remove-text-properties next pos '(display nil face nil) str))))))
+      (advice-add #'ivy-cleanup-string :after #'my-ivy-cleanup-indentation)))
 
 ;; Colorize color names in buffers
 (use-package rainbow-mode
