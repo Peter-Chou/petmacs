25b95601941e5927e4b7e1ecbd6b5de81bec05f9
Peter-Chou <zhou_00009@126.com>
2019-08-14 09:19:43 +0800

    add more popup windows's rules

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -127,6 +127,8 @@
           ("*lsp session*" :size 0.4 :align 'below :autoclose t)
           (" *Org todo*" :select t :size 4 :align 'below :autoclose t)
           ("*Org Dashboard*" :select t :size 0.4 :align 'below :autoclose t)
+	  ("^\\*macro expansion\\**" :regexp t :size 0.4 :align 'below)
+          (" *Install vterm" :size 0.3 :align 'below)
 
           (ag-mode :select t :align 'below)
           (grep-mode :select t :align 'below)
