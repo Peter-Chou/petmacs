18c31f52ce83345a59091968658ea53f807cba12
Peter-Chou <zhou_00009@126.com>
2019-10-04 09:31:37 +0800

    update all-the-icons config

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -31,18 +31,23 @@
   :init (unless (or sys/win32p (member "all-the-icons" (font-family-list)))
           (all-the-icons-install-fonts t))
   :config
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(vterm-mode all-the-icons-octicon "terminal" :v-adjust 0.2))
   (add-to-list 'all-the-icons-icon-alist
                '("\\.xpm$" all-the-icons-octicon "file-media" :v-adjust 0.0 :face all-the-icons-dgreen))
+
+  (add-to-list 'all-the-icons-icon-alist
+               '("\\.toml$" all-the-icons-octicon "settings" :v-adjust 0.0 :face all-the-icons-dyellow))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(conf-toml-mode all-the-icons-octicon "settings" :v-adjust 0.0 :face all-the-icons-dyellow))
   (add-to-list 'all-the-icons-icon-alist
                '("\\.lua$" all-the-icons-fileicon "lua" :face all-the-icons-dblue))
   (add-to-list 'all-the-icons-mode-icon-alist
                '(lua-mode all-the-icons-fileicon "lua" :face all-the-icons-dblue))
-  (add-to-list 'all-the-icons-icon-alist
-               '("\\.go$" all-the-icons-fileicon "go" :face all-the-icons-blue))
-  (add-to-list 'all-the-icons-mode-icon-alist
-               '(go-mode all-the-icons-fileicon "go" :face all-the-icons-blue))
   (add-to-list 'all-the-icons-mode-icon-alist
                '(help-mode all-the-icons-faicon "info-circle" :height 1.1 :v-adjust -0.1 :face all-the-icons-purple))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(helpful-mode all-the-icons-faicon "info-circle" :height 1.1 :v-adjust -0.1 :face all-the-icons-purple))
   (add-to-list 'all-the-icons-mode-icon-alist
                '(Info-mode all-the-icons-faicon "info-circle" :height 1.1 :v-adjust -0.1))
   (add-to-list 'all-the-icons-icon-alist
@@ -53,12 +58,18 @@
                '(cask-mode all-the-icons-fileicon "elisp" :height 1.0 :v-adjust -0.2 :face all-the-icons-blue))
   (add-to-list 'all-the-icons-icon-alist
                '(".*\\.ipynb\\'" all-the-icons-fileicon "jupyter" :height 1.2 :face all-the-icons-orange))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(ein:notebooklist-mode all-the-icons-faicon "book" :face all-the-icons-orange))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(ein:notebook-mode all-the-icons-fileicon "jupyter" :height 1.2 :face all-the-icons-orange))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(ein:notebook-multilang-mode all-the-icons-fileicon "jupyter" :height 1.2 :face all-the-icons-orange))
   (add-to-list 'all-the-icons-icon-alist
                '("\\.epub\\'" all-the-icons-faicon "book" :height 1.0 :v-adjust -0.1 :face all-the-icons-green))
   (add-to-list 'all-the-icons-mode-icon-alist
                '(nov-mode all-the-icons-faicon "book" :height 1.0 :v-adjust -0.1 :face all-the-icons-green))
   (add-to-list 'all-the-icons-mode-icon-alist
-               '(gfm-mode  all-the-icons-octicon "markdown" :face all-the-icons-blue)))
+               '(gfm-mode all-the-icons-octicon "markdown" :face all-the-icons-lblue)))
 
 (use-package font-lock+
   :quelpa
