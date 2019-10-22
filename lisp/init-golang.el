bbd791372f3458949a1a80c544858835c40d8517
Peter-Chou <zhou_00009@126.com>
2019-10-22 12:21:56 +0800

    add go-lang

---
+++ lisp/init-golang.el
@@ -0,0 +1,121 @@
+;; init-golang.el --- Setup Golang IDE.  -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(eval-when-compile
+  (require 'init-const)
+  (require 'init-custom))
+
+;; Golang
+(use-package go-mode
+  :functions (go-packages-gopkgs go-update-tools)
+  :bind (:map go-mode-map
+         ([remap xref-find-definitions] . godef-jump)
+         ("C-c R" . go-remove-unused-imports)
+         ("<f1>" . godoc-at-point))
+  :config
+  ;; Env vars
+  (with-eval-after-load 'exec-path-from-shell
+    (exec-path-from-shell-copy-envs '("GOPATH" "GO111MODULE" "GOPROXY")))
+
+  ;; Format with `goimports' if possible, otherwise using `gofmt'
+  (when (executable-find "goimports")
+    (setq gofmt-command "goimports"))
+  (add-hook 'before-save-hook #'gofmt-before-save)
+
+  ;; Install or update tools
+  (defvar go--tools '("golang.org/x/tools/cmd/goimports"
+                      "golang.org/x/tools/cmd/gorename"
+
+                      ;; "github.com/rogpeppe/godef"
+                      "github.com/go-delve/delve/cmd/dlv"
+                      "github.com/josharian/impl"
+                      "github.com/cweill/gotests/..."
+                      "github.com/fatih/gomodifytags"
+                      "github.com/davidrjenni/reftools/cmd/fillstruct"
+                      "github.com/golangci/golangci-lint/cmd/golangci-lint")
+    "All necessary go tools.")
+
+  ;; Do not use the -u flag for gopls, as it will update the dependencies to incompatible versions
+  ;; https://github.com/golang/tools/blob/master/gopls/doc/user.md#installation
+  (defvar go--tools-no-update '("golang.org/x/tools/gopls@latest")
+    "All necessary go tools without update the dependencies.")
+
+  (defun go-update-tools ()
+    "Install or update go tools."
+    (interactive)
+    (unless (executable-find "go")
+      (user-error "Unable to find `go' in `exec-path'!"))
+
+    (message "Installing go tools...")
+    (let ((proc-name "go-tools")
+          (proc-buffer "*Go Tools*"))
+      (dolist (pkg go--tools-no-update)
+        (set-process-sentinel
+         (start-process proc-name proc-buffer "go" "get" "-v" pkg)
+         (lambda (proc _)
+           (let ((status (process-exit-status proc)))
+             (if (= 0 status)
+                 (message "Installed %s" pkg)
+               (message "Failed to install %s: %d" pkg status))))))
+
+      (dolist (pkg go--tools)
+        (set-process-sentinel
+         (start-process proc-name proc-buffer "go" "get" "-u" "-v" pkg)
+         (lambda (proc _)
+           (let ((status (process-exit-status proc)))
+             (if (= 0 status)
+                 (message "Installed %s" pkg)
+               (message "Failed to install %s: %d" pkg status))))))))
+
+  ;; Try to install go tools if `gopls' is not found
+  (unless (executable-find "gopls")
+    (go-update-tools))
+
+  ;; Misc
+  (use-package go-dlv)
+  (use-package go-fill-struct)
+  (use-package go-impl)
+  (use-package go-rename)
+
+  ;; Install: go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
+  (use-package flycheck-golangci-lint
+    :if (executable-find "golangci-lint")
+    :after flycheck
+    :defines flycheck-disabled-checkers
+    :hook (go-mode . (lambda ()
+                       "Enable golangci-lint."
+                       (setq flycheck-disabled-checkers '(go-gofmt
+                                                          go-golint
+                                                          go-vet
+                                                          go-build
+                                                          go-test
+                                                          go-errcheck))
+                       (flycheck-golangci-lint-setup))))
+
+  (use-package go-tag
+    :bind (:map go-mode-map
+           ("C-c t" . go-tag-add)
+           ("C-c T" . go-tag-remove))
+    :config (setq go-tag-args (list "-transform" "camelcase")))
+
+  (use-package go-gen-test
+    :bind (:map go-mode-map
+           ("C-c C-t" . go-gen-test-dwim)))
+
+  (use-package gotest
+    :bind (:map go-mode-map
+           ("C-c a" . go-test-current-project)
+           ("C-c m" . go-test-current-file)
+           ("C-c ." . go-test-current-test)
+           ("C-c x" . go-run))))
+
+;; Local Golang playground for short snippets
+(use-package go-playground
+  :diminish)
+
+(provide 'init-golang)
+
+;;; init-golang.el ends here
