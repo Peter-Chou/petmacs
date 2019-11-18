e22f69ef20aed4e17017d425935ea2709c30ebff
Peter-Chou <zhou_00009@126.com>
2019-11-18 09:49:19 +0800

    update

--- lisp/init-layout.el
+++ lisp/init-layout.el
@@ -4,7 +4,6 @@
 
 ;;; Code:
 
-;; Windows/buffers sets shared among frames + save/load.
 (use-package persp-mode
   :diminish
   :defines (recentf-exclude ivy-ignore-buffers ivy-sort-functions-alist)
@@ -19,15 +18,20 @@
               persp-common-buffer-filter-functions
               (list #'(lambda (b)
                         "Ignore temporary buffers."
-                        (or (string-prefix-p " " (buffer-name b))
-                            (and (string-prefix-p "*" (buffer-name b))
-                                 (not (string-equal "*scratch*" (buffer-name b))))
-                            (string-prefix-p "magit" (buffer-name b))
-                            (string-prefix-p "Pfuture-Callback" (buffer-name b))
-                            (eq (buffer-local-value 'major-mode b) 'erc-mode)
-                            (eq (buffer-local-value 'major-mode b) 'rcirc-mode)
-                            (eq (buffer-local-value 'major-mode b) 'nov-mode)
-                            (eq (buffer-local-value 'major-mode b) 'vterm-mode)))))
+                        (let ((bname (file-name-nondirectory (buffer-name b))))
+                          (or (string-prefix-p " " bname)
+                              (and (string-prefix-p "*" bname)
+                                   (not (string-equal "*scratch*" bname)))
+                              (string-suffix-p ".elc" bname)
+                              (string-suffix-p ".gz" bname)
+                              (string-suffix-p ".zip" bname)
+                              (string-prefix-p "magit" bname)
+                              (string-prefix-p "Pfuture-Callback" bname)
+                              (string-prefix-p "magit" bname)
+                              (eq (buffer-local-value 'major-mode b) 'erc-mode)
+                              (eq (buffer-local-value 'major-mode b) 'rcirc-mode)
+                              (eq (buffer-local-value 'major-mode b) 'nov-mode)
+                              (eq (buffer-local-value 'major-mode b) 'vterm-mode))))))
   :config
   ;; Don't save persp configs in `recentf'
   (push persp-save-dir recentf-exclude)
