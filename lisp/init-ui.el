9f43b6afa5d831fc86f1550f293c8c01a825408f
Peter-Chou <zhou_00009@126.com>
2019-12-30 09:22:51 +0800

    improve all-the-icons setting

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -28,10 +28,28 @@
 ;; NOTE: Must run `M-x all-the-icons-install-fonts', and install fonts manually on Windows
 (use-package all-the-icons
   :if (display-graphic-p)
-  :init (unless (or sys/win32p (member "all-the-icons" (font-family-list)))
+  :init (unless (or sys/win32p (font-installed-p "all-the-icons"))
           (all-the-icons-install-fonts t))
   :config
   (with-no-warnings
+    ;; FIXME: Align the directory icons
+    ;; @see https://github.com/domtronn/all-the-icons.el/pull/173
+    (defun all-the-icons-icon-for-dir (dir &optional chevron padding)
+      "Format an icon for DIR with CHEVRON similar to tree based directories."
+      (let* ((matcher (all-the-icons-match-to-alist (file-name-base (directory-file-name dir)) all-the-icons-dir-icon-alist))
+             (path (expand-file-name dir))
+             (chevron (if chevron (all-the-icons-octicon (format "chevron-%s" chevron) :height 0.8 :v-adjust -0.1) ""))
+             (padding (or padding "\t"))
+             (icon (cond
+                    ((file-symlink-p path)
+                     (all-the-icons-octicon "file-symlink-directory" :height 1.0 :v-adjust 0.0))
+                    ((all-the-icons-dir-is-submodule path)
+                     (all-the-icons-octicon "file-submodule" :height 1.0 :v-adjust 0.0))
+                    ((file-exists-p (format "%s/.git" path))
+                     (format "%s" (all-the-icons-octicon "repo" :height 1.1 :v-adjust 0.0)))
+                    (t (apply (car matcher) (list (cadr matcher) :v-adjust 0.0))))))
+        (format "%s%s%s%s%s" padding chevron padding icon padding)))
+
     (defun all-the-icons-reset ()
       "Reset (unmemoize/memoize) the icons."
       (interactive)
@@ -46,6 +64,32 @@
           (memoize f)))
       (message "Reset all-the-icons")))
 
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(bongo-playlist-mode all-the-icons-material "playlist_play" :height 1.2 :v-adjust -0.2 :face 'all-the-icons-green))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(bongo-library-mode all-the-icons-material "library_music" :height 1.1 :v-adjust -0.2 :face 'all-the-icons-dgreen))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(gnus-group-mode all-the-icons-fileicon "gnu" :face 'all-the-icons-silver))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(gnus-summary-mode all-the-icons-octicon "inbox" :height 1.0 :v-adjust 0.0 :face 'all-the-icons-orange))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(gnus-article-mode all-the-icons-octicon "mail" :height 1.1 :v-adjust 0.0 :face 'all-the-icons-lblue))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(diff-mode all-the-icons-octicon "git-compare" :v-adjust 0.0 :face all-the-icons-lred))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(flycheck-error-list-mode all-the-icons-octicon "checklist" :height 1.1 :v-adjust 0.0 :face all-the-icons-lred))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(elfeed-search-mode all-the-icons-faicon "rss-square" :v-adjust -0.1 :face all-the-icons-orange))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(elfeed-show-mode all-the-icons-octicon "rss" :height 1.1 :v-adjust 0.0 :face all-the-icons-lorange))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(newsticker-mode all-the-icons-faicon "rss-square" :v-adjust -0.1 :face all-the-icons-orange))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(newsticker-treeview-mode all-the-icons-faicon "rss-square" :v-adjust -0.1 :face all-the-icons-orange))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(newsticker-treeview-list-mode all-the-icons-octicon "rss" :height 1.1 :v-adjust 0.0 :face all-the-icons-orange))
+  (add-to-list 'all-the-icons-mode-icon-alist
+               '(newsticker-treeview-item-mode all-the-icons-octicon "rss" :height 1.1 :v-adjust 0.0 :face all-the-icons-lorange))
   (add-to-list 'all-the-icons-icon-alist
                '("\\.[bB][iI][nN]$" all-the-icons-octicon "file-binary" :v-adjust 0.0 :face all-the-icons-yellow))
   (add-to-list 'all-the-icons-icon-alist
