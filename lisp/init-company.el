49e0f784f9f4abbb27ae8399776cd7831ec5ccdf
Peter-Chou <zhou_00009@126.com>
2020-03-27 09:34:09 +0800

    revert company config

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -11,79 +11,41 @@
 (use-package company
   :diminish
   :defines (company-dabbrev-ignore-case company-dabbrev-downcase)
-  :commands company-cancel
+  :commands company-abort
   :bind (("M-/" . company-complete)
-         ("C-M-i" . company-complete)
-         :map company-mode-map
-         ("<backtab>" . company-yasnippet)
-         :map company-active-map
-         ("C-p" . company-select-previous)
-         ("C-n" . company-select-next)
-         ("<tab>" . company-complete-common-or-cycle)
-         ("<backtab>" . my-company-yasnippet)
-         :map company-search-map
-         ("C-p" . company-select-previous)
-         ("C-n" . company-select-next))
+	 ("<backtab>" . company-yasnippet)
+	 :map company-active-map
+	 ("C-p" . company-select-previous)
+	 ("C-n" . company-select-next)
+	 ("<tab>" . company-complete-selection)
+	 ("C-/" . counsel-company)
+	 ;; ("C-M-/" . company-filter-candidates)
+	 ("C-d" . company-show-doc-buffer)
+	 :map company-search-map
+	 ("C-p" . company-select-previous)
+	 ("C-n" . company-select-next)
+	 ;; ("C-/" . company-search-candidates)
+	 ;; ("C-M-/" . company-filter-candidates)
+	 ("C-d" . company-show-doc-buffer))
   :hook (after-init . global-company-mode)
-  :init
-  (setq company-tooltip-align-annotations t
-        company-tooltip-limit 12
-        company-idle-delay 0
-        company-echo-delay (if (display-graphic-p) nil 0)
-        company-backends '((company-files          ; files & directory
-			    company-keywords       ; keywords
-			    )
-			   (company-abbrev company-dabbrev))
-        company-minimum-prefix-length 2
-        company-require-match nil
+  :config
+  (setq company-tooltip-align-annotations t ; aligns annotation to the right
+	company-tooltip-limit 12            ; bigger popup window
+	company-idle-delay .1               ; decrease delay before autocompletion popup shows
+	company-echo-delay (if (display-graphic-p) nil 0)
+	company-minimum-prefix-length 2
+	company-require-match nil
         company-dabbrev-ignore-case nil
         company-dabbrev-downcase nil
-        company-global-modes '(not erc-mode message-mode help-mode
-                                   gud-mode eshell-mode shell-mode)
-        company-frontends '(company-pseudo-tooltip-frontend
+        company-global-modes '(not erc-mode message-mode help-mode gud-mode eshell-mode shell-mode)
+	company-backends '((company-files          ; files & directory
+			    company-keywords       ; keywords
+			    company-capf
+			    company-yasnippet)
+			   (company-abbrev company-dabbrev))
+company-frontends '(company-pseudo-tooltip-frontend
                             company-echo-metadata-frontend))
-
-  (defun my-company-yasnippet ()
-    "Hide the current completeions and show snippets."
-    (interactive)
-    (company-cancel)
-    (call-interactively 'company-yasnippet))
-  :config
-  ;; `yasnippet' integration
-  (with-no-warnings
-    (with-eval-after-load 'yasnippet
-      (defun company-backend-with-yas (backend)
-        "Add `yasnippet' to company backend."
-        (if (and (listp backend) (member 'company-yasnippet backend))
-            backend
-          (append (if (consp backend) backend (list backend))
-                  '(:with company-yasnippet))))
-
-      (defun my-company-enbale-yas (&rest _)
-        "Enable `yasnippet' in `company'."
-        (setq company-backends (mapcar #'company-backend-with-yas company-backends)))
-      ;; Enable in current backends
-      (my-company-enbale-yas)
-      ;; Support `company-lsp'
-      (advice-add #'lsp--auto-configure :after #'my-company-enbale-yas)
-
-      (defun my-company-yasnippet-disable-inline (fun command &optional arg &rest _ignore)
-        "Enable yasnippet but disable it inline."
-        (if (eq command 'prefix)
-            (when-let ((prefix (funcall fun 'prefix)))
-              (unless (memq (char-before (- (point) (length prefix)))
-                            '(?. ?> ?\( ?\) ?\[ ?{ ?} ?\" ?' ?`))
-                prefix))
-          (progn
-            (when (and (bound-and-true-p lsp-mode)
-                       arg (not (get-text-property 0 'yas-annotation-patch arg)))
-              (let* ((name (get-text-property 0 'yas-annotation arg))
-                     (snip (format "%s (Snippet)" name))
-                     (len (length arg)))
-                (put-text-property 0 len 'yas-annotation snip arg)
-                (put-text-property 0 len 'yas-annotation-patch t arg)))
-            (funcall fun command arg))))
-      (advice-add #'company-yasnippet :around #'my-company-yasnippet-disable-inline)))
+  (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
 
   ;; Better sorting and filtering
   (use-package company-prescient
