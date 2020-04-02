d389987928f059203456f3156f305eb72dfd251e
Peter-Chou <zhou_00009@126.com>
2020-04-02 21:04:17 +0800

    add sbt mode

--- lisp/init-scala.el
+++ lisp/init-scala.el
@@ -14,7 +14,25 @@
   :hook (scala-mode . (lambda ()
 			(require 'lsp-metals)
 			(lsp-deferred)))
-  :mode "\\.s\\(cala\\|bt\\)$")
+  :mode "\\.s\\(cala\\|bt\\)$"
+  :config
+  (with-eval-after-load 'lsp-treemacs
+    (lsp-metals-treeview-enable t)
+    (setq lsp-metals-treeview-show-when-views-received t)))
+
+;; Enable sbt mode for executing sbt commands
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+   (setq sbt:program-options '("-Dsbt.supershell=false"))
+)
 
 (provide 'init-scala)
 
