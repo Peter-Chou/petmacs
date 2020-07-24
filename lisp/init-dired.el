e697c2f5107c2fb6bdeaab6518775cded4f84cb6
Peter-Chou <zhou_00009@126.com>
2020-07-24 11:45:45 +0800

    update all-the-icons-dired

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -66,37 +66,43 @@
   )
 
 ;; Quick sort dired buffers via hydra
-  ;; Shows icons
-  (use-package all-the-icons-dired
-    :diminish
-    :if (icons-displayable-p)
-    :hook (dired-mode . all-the-icons-dired-mode)
-    :config
+;; Shows icons
+(use-package all-the-icons-dired
+  :diminish
+  :if (icons-displayable-p)
+  :hook (dired-mode . all-the-icons-dired-mode)
+  :config
+  ;; FIXME: Refresh after creating or renaming the files/directories.
+  ;; @see https://github.com/jtbm37/all-the-icons-dired/issues/34.
+  (with-no-warnings
+    (advice-add #'dired-do-create-files :around #'all-the-icons-dired--refresh-advice)
+    (advice-add #'dired-create-directory :around #'all-the-icons-dired--refresh-advice)
+    (advice-add #'wdired-abort-changes :around #'all-the-icons-dired--refresh-advice))
 
-    (with-no-warnings
-      (defun my-all-the-icons-dired--refresh ()
-        "Display the icons of files in a dired buffer."
-        (all-the-icons-dired--remove-all-overlays)
-        ;; NOTE: don't display icons it too many items
-        (if (<= (count-lines (point-min) (point-max)) 1000)
-            (save-excursion
-              ;; TRICK: Use TAB to align icons
-              (setq-local tab-width 1)
-
-              (goto-char (point-min))
-              (while (not (eobp))
-                (let ((file (dired-get-filename 'verbatim t)))
+  (with-no-warnings
+    (defun my-all-the-icons-dired--refresh ()
+      "Display the icons of files in a dired buffer."
+      (all-the-icons-dired--remove-all-overlays)
+      ;; NOTE: don't display icons it too many items
+      (if (<= (count-lines (point-min) (point-max)) 1000)
+          (save-excursion
+            (goto-char (point-min))
+            (while (not (eobp))
+              (when (dired-move-to-filename nil)
+                (let ((file (file-local-name (dired-get-filename 'relative 'noerror))))
                   (when file
                     (let ((icon (if (file-directory-p file)
-                                    (all-the-icons-icon-for-dir file nil "")
-                                  (all-the-icons-icon-for-file file :v-adjust 0.0))))
+                                    (all-the-icons-icon-for-dir file
+                                                                :face 'all-the-icons-dired-dir-face
+                                                                :height 0.9
+                                                                :v-adjust all-the-icons-dired-v-adjust)
+                                  (all-the-icons-icon-for-file file :height 0.9 :v-adjust all-the-icons-dired-v-adjust))))
                       (if (member file '("." ".."))
                           (all-the-icons-dired--add-overlay (point) "  \t")
-                        (all-the-icons-dired--add-overlay (point) (concat icon "\t"))))))
-                (dired-next-line 1)))
-          (message "Not display icons because of too many items.")))
-      (advice-add #'all-the-icons-dired--refresh
-                  :override #'my-all-the-icons-dired--refresh)))
+                        (all-the-icons-dired--add-overlay (point) (concat icon "\t")))))))
+              (forward-line 1)))
+        (message "Not display icons because of too many items.")))
+    (advice-add #'all-the-icons-dired--refresh :override #'my-all-the-icons-dired--refresh)))
 
 ;; Quick sort dired buffers via hydra
 (use-package dired-quick-sort
