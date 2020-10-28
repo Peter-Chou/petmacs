6b87c00555f19b76f72d84cc37925febb745d636
Peter-Chou <zhou_00009@126.com>
2020-10-28 08:45:28 +0800

    :lipstick:

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; init.el --- Petmacs configurations  -*- lexical-binding: t no-byte-compile: t; -*-
+;;; init.el --- Petmacs configurations  -*- lexical-binding: t no-byte-compile: t -*-
 
 ;;; Commentary:
 
