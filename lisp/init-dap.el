03eee3884a7d303115dc8229359b37705331b08f
Peter-Chou <zhou_00009@126.com>
2021-04-09 22:08:32 +0800

    update dap mode

--- lisp/init-dap.el
+++ lisp/init-dap.el
@@ -10,43 +10,20 @@
 ;; C++: follow instruction from https://github.com/llvm-mirror/lldb/tree/master/tools/lldb-vscode
 ;; `lsp-mode' and `treemacs' integration
 (use-package dap-mode
-  :preface
-  (defun petmacs--autoload-dap-templates()
-    "autoload dap templates in projectile_root_dir/dap_templates.el"
-    (interactive)
-    (let* ((pdir (projectile-project-root))
-	   (pfile (concat pdir "dap_templates.el")))
-      (when (file-exists-p pfile)
-	(with-temp-buffer
-	  (insert-file-contents pfile)
-	  (eval-buffer)
-	  (message "dap templates has been loaded.")))))
-  (defun petmacs/register-dap-degbug-template ()
-    (interactive)
-    (require 'dap-python)
-    (dap-register-debug-template "python-debug"
-				 (list :type "python"
-                                       :args "-i"
-                                       :cwd nil
-                                       :env '(("DEBUG" . "1"))
-                                       :target-module nil
-                                       :request "launch"
-                                       :name "python-debug")))
   :diminish
   :defines dap-python-executable
-  :functions dap-hydra/nil
   :diminish
   :bind (:map lsp-mode-map
          ("<f5>" . dap-debug)
          ("M-<f5>" . dap-hydra))
   :hook ((after-init . dap-auto-configure-mode)
-         (dap-stopped . (lambda (_args) (dap-hydra)))
-         (dap-terminated . (lambda (_args) (dap-hydra/nil)))
+         ;; (dap-stopped . (lambda (arg) (call-interactively #'dap-hydra)))
 	 ((c-mode c++-mode objc-mode swift-mode) . (lambda () (require 'dap-lldb)))
          (python-mode . (lambda () (require 'dap-python)))
+	 ;;;; go install github.com/go-delve/delve/cmd/dlv@latest
          (go-mode . (lambda () (require 'dap-go)))
          (java-mode . (lambda () (require 'dap-java)))
-         ;; ((js-mode js2-mode) . (lambda () (require 'dap-chrome)))
+         ((js-mode js2-mode) . (lambda () (require 'dap-chrome)))
 	 )
   :init
   (require 'cl-lib)
