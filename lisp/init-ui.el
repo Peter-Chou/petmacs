7853091b988f6fc46f63a3ea833cc9325f6e2a22
Peter-Chou <zhou_00009@126.com>
2021-05-31 13:28:00 +0800

    :lipstick:

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -51,9 +51,8 @@
 ;; Make certain buffers grossly incandescent
 ;; Must before loading the theme
 (use-package solaire-mode
-  :hook (((change-major-mode after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
-         (minibuffer-setup . solaire-mode-in-minibuffer))
-  :init (solaire-global-mode 1))
+  :hook (after-load-theme . solaire-global-mode))
+
 ;; Icons
 ;; NOTE: Must run `M-x all-the-icons-install-fonts', and install fonts manually on Windows
 (use-package all-the-icons
