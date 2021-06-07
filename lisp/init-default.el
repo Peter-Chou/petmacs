ac569d688ec5815f885fb786f0464ddb35ff74ae
Peter-Chou <zhou_00009@126.com>
2021-06-07 08:58:53 +0800

    update ui

--- lisp/init-default.el
+++ lisp/init-default.el
@@ -127,15 +127,6 @@
 (when (boundp 'x-gtk-use-system-tooltips)
   (setq x-gtk-use-system-tooltips nil))
 
-;; Mouse & Smooth Scroll
-;; Scroll one line at a time (less "jumpy" than defaults)
-(when (display-graphic-p)
-  (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))
-        mouse-wheel-progressive-speed nil))
-(setq scroll-step 1
-      scroll-margin 0
-      scroll-conservatively 100000)
-
 ;; Basic modes
 
 ;; Start server
