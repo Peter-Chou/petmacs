3739e063729777491f3289423ec131263152bf1d
Peter-Chou <zhou_00009@126.com>
2021-06-24 15:14:41 +0800

    update

--- lisp/init-java.el
+++ lisp/init-java.el
@@ -8,7 +8,6 @@
   (require 'init-const)
   (require 'init-custom))
 
-;; latest jdtls requires java >= 11 to work
 (use-package lsp-java
   :hook (java-mode . (lambda ()
 		       (require 'lsp-java)
@@ -17,7 +16,8 @@
   (setq lsp-java-import-maven-enabled t
 	lsp-java-implementations-code-lens-enabled t
 	lsp-java-save-actions-organize-imports t
-	;; lsp-java-java-path "/opt/jdk11/bin/java"
+	;; latest jdtls requires java >= 11 to work
+	lsp-java-java-path "/opt/jdk11/bin/java"
 	lsp-java-vmargs '("-XX:+UseParallelGC" "-XX:GCTimeRatio=4" "-XX:AdaptiveSizePolicyWeight=90" "-Dsun.zip.disableMemoryMapping=true" "-Xmx6G" "-Xms100m")
 	;; Runtime name must be one of: “J2SE-1.5”, “JavaSE-1.6”, “JavaSE-1.7”, “JavaSE-1.8” etc
 	;; lsp-java-configuration-runtimes '[(:name "JavaSE-1.8"
