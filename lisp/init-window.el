f1f21cca0ad15f0aba428441c5c5662547dc4297
Peter-Chou <zhou_00009@126.com>
2021-08-27 09:05:02 +0800

    update shackle

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -33,82 +33,10 @@
 (defvar-local shackle--current-popup-window nil) ; current popup window
 (put 'shackle--current-popup-window 'permanent-local t)
 
+;; Enforce rules for popups
 (use-package shackle
-  :preface
-  (defun petmacs/shackle-popup-message-buffer ()
-    "View message buffer."
-    (interactive)
-    (ignore-errors
-      (display-buffer "*Messages*")))
-
-  (defun petmacs/shackle-popup-compilation-buffer ()
-    "View compilation buffer."
-    (interactive)
-    (ignore-errors
-      (display-buffer"*compilation*")))
-
-  :functions org-switch-to-buffer-other-window
-  :commands shackle-display-buffer
   :hook (after-init . shackle-mode)
-  :config
-  (with-no-warnings
-    (defvar shackle--popup-window-list nil) ; all popup windows
-    (defvar-local shackle--current-popup-window nil) ; current popup window
-    (put 'shackle--current-popup-window 'permanent-local t)
-
-    (defun shackle-last-popup-buffer ()
-      "View last popup buffer."
-      (interactive)
-      (ignore-errors
-        (display-buffer shackle-last-buffer)))
-    (bind-key "C-h z" #'shackle-last-popup-buffer)
-
-    ;; Add keyword: `autoclose'
-    (defun shackle-display-buffer-hack (fn buffer alist plist)
-      (let ((window (funcall fn buffer alist plist)))
-        (setq shackle--current-popup-window window)
-
-        (when (plist-get plist :autoclose)
-          (push (cons window buffer) shackle--popup-window-list))
-        window))
-
-    (defun shackle-close-popup-window-hack (&rest _)
-      "Close current popup window via `C-g'."
-      (setq shackle--popup-window-list
-            (cl-loop for (window . buffer) in shackle--popup-window-list
-                     if (and (window-live-p window)
-                             (equal (window-buffer window) buffer))
-                     collect (cons window buffer)))
-      ;; `C-g' can deactivate region
-      (when (and (called-interactively-p 'interactive)
-                 (not (region-active-p)))
-        (let (window buffer process)
-          (if (one-window-p)
-              (progn
-                (setq window (selected-window))
-                (when (equal (buffer-local-value 'shackle--current-popup-window
-                                                 (window-buffer window))
-                             window)
-                  (winner-undo)))
-            (progn
-              (setq window (caar shackle--popup-window-list))
-              (setq buffer (cdar shackle--popup-window-list))
-              (when (and (window-live-p window)
-                         (equal (window-buffer window) buffer))
-                (setq process (get-buffer-process buffer))
-                (when (process-live-p process)
-                  (kill-process process))
-                (delete-window window)
-
-                (pop shackle--popup-window-list)))))))
-
-    (advice-add #'keyboard-quit :before #'shackle-close-popup-window-hack)
-    (advice-add #'shackle-display-buffer :around #'shackle-display-buffer-hack))
-
-  ;; HACK: compatibility issue with `org-switch-to-buffer-other-window'
-  (advice-add #'org-switch-to-buffer-other-window :override #'switch-to-buffer-other-window)
-
-  ;; rules
+  :init
   (setq shackle-default-size 0.4
         shackle-default-alignment 'below
         shackle-default-rule nil
@@ -136,12 +64,14 @@
           ("*Finder*" :select t :size 0.3 :align 'below :autoclose t)
           ("^\\*macro expansion\\**" :regexp t :size 0.4 :align 'below)
           ("^\\*elfeed-entry" :regexp t :size 0.7 :align 'below :autoclose t)
-          ((" *Org todo*" "*Org Dashboard*" "*Org Select*") :select t :size 0.4 :align 'below :autoclose t)
           (" *Install vterm* " :size 0.35 :same t :align 'below)
           (("*Paradox Report*" "*package update results*") :size 0.2 :align 'below :autoclose t)
           ("*Package-Lint*" :size 0.4 :align 'below :autoclose t)
-          (("*Gofmt Errors*" "*Go Test*") :select t :size 0.3 :align 'below :autoclose t)
           ("*How Do You*" :select t :size 0.5 :align 'below :autoclose t)
+
+          (("*Org Agenda*" " *Agenda Commands*" " *Org todo*" "*Org Dashboard*" "*Org Select*")
+           :select t :size 0.1 :align 'below :autoclose t)
+          (("\\*Capture\\*" "^CAPTURE-.*\\.org*") :regexp t :select t :size 0.3 :align 'below :autoclose t)
 
           ("*ert*" :size 15 :align 'below :autoclose t)
           (overseer-buffer-mode :size 15 :align 'below :autoclose t)
@@ -154,7 +84,8 @@
           ("*DAP Templates*" :select t :size 0.4 :align 'below :autoclose t)
           (dap-server-log-mode :size 15 :align 'below :autoclose t)
           ("*rustfmt*" :select t :size 0.3 :align 'below :autoclose t)
-          ((rustic-compilation-mode rustic-cargo-clippy-mode rustic-cargo-outdated-mode rustic-cargo-test-mode) :select t :size 0.3 :align 'below :autoclose t)
+          ((rustic-compilation-mode rustic-cargo-clippy-mode rustic-cargo-outdated-mode rustic-cargo-test-mode)
+           :select t :size 0.3 :align 'below :autoclose t)
 
           (profiler-report-mode :select t :size 0.5 :align 'below)
           ("*ELP Profiling Restuls*" :select t :size 0.5 :align 'below)
@@ -162,13 +93,74 @@
           ((inferior-python-mode inf-ruby-mode swift-repl-mode) :size 0.4 :align 'below)
           ("*prolog*" :size 0.4 :align 'below)
 
-	  ((grep-mode occur-mode rg-mode deadgrep-mode ag-mode pt-mode) :select t :size 0.4 :align 'below)
-          (Buffer-menu-mode :select t :size 20 :align 'below :autoclose t)
+          (("*Gofmt Errors*" "*Go Test*") :select t :size 0.3 :align 'below :autoclose t)
+          (godoc-mode :select t :size 0.4 :align 'below :autoclose t)
+
+          ((grep-mode occur-mode rg-mode deadgrep-mode ag-mode pt-mode) :select t :size 0.4 :align 'below)
+          (Buffer-menu-mode :select t :size 0.5 :align 'below :autoclose t)
           (gnus-article-mode :select t :size 0.7 :align 'below :autoclose t)
           (helpful-mode :select t :size 0.3 :align 'below :autoclose t)
-	  ((process-menu-mode list-environment-mode cargo-process-mode) :select t :size 0.3 :align 'below)
-          (list-environment-mode :select t :size 0.3 :align 'below :autoclose t)
-          (tabulated-list-mode :size 0.4 :align 'below))))
+          (devdocs-mode :select t :size 0.4 :align 'below :autoclose t)
+          ((process-menu-mode list-environment-mode cargo-process-mode) :select t :size 0.3 :align 'below)
+          (("*docker-containers*" "*docker-images*" "*docker-networks*" "*docker-volumes*")
+           :size 0.4 :align 'below :autoclose t)
+          (bookmark-bmenu-mode :select t :size 0.4 :align 'below)
+          (tabulated-list-mode :size 0.4 :align 'below :autclose t)))
+  :config
+  (with-no-warnings
+    (defvar shackle--popup-window-list nil
+      "All popup windows.")
+    (defvar-local shackle--current-popup-window nil
+      "Current popup window.")
+    (put 'shackle--current-popup-window 'permanent-local t)
+
+    (defun shackle-last-popup-buffer ()
+      "View last popup buffer."
+      (interactive)
+      (ignore-errors
+        (display-buffer shackle-last-buffer)))
+    (bind-key "C-h z" #'shackle-last-popup-buffer)
+
+    ;; Add keyword: `autoclose'
+    (defun shackle-display-buffer-hack (fn buffer alist plist)
+      (let ((window (funcall fn buffer alist plist)))
+        (setq shackle--current-popup-window window)
+
+        (when (plist-get plist :autoclose)
+          (push (cons window buffer) shackle--popup-window-list))
+        window))
+    (advice-add #'shackle-display-buffer :around #'shackle-display-buffer-hack)
+
+    (defun shackle-close-popup-window-hack (&rest _)
+      "Close current popup window via `C-g'."
+      (setq shackle--popup-window-list
+            (cl-loop for (window . buffer) in shackle--popup-window-list
+                     if (and (window-live-p window)
+                             (equal (window-buffer window) buffer))
+                     collect (cons window buffer)))
+      ;; `C-g' can deactivate region
+      (when (and (called-interactively-p 'interactive)
+                 (not (region-active-p)))
+        (if (one-window-p)
+            (let ((window (selected-window)))
+              (when (equal (buffer-local-value 'shackle--current-popup-window
+                                               (window-buffer window))
+                           window)
+                (winner-undo)))
+          (let* ((window (caar shackle--popup-window-list))
+                 (buffer (cdar shackle--popup-window-list))
+                 (process (get-buffer-process buffer)))
+            (when (and (window-live-p window)
+                       (equal (window-buffer window) buffer))
+              (when (process-live-p process)
+                (kill-process process))
+              (delete-window window)
+              (pop shackle--popup-window-list))))))
+    (advice-add #'keyboard-quit :before #'shackle-close-popup-window-hack)
+
+    ;; Compatible with org
+    (advice-add #'org-switch-to-buffer-other-window
+                :override #'switch-to-buffer-other-window)))
 
 ;; center window
 (use-package olivetti
