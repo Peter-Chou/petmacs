4cfc5e97287a5c2bd6ed17b9c8ec5328b40e9ee5
Peter-Chou <zhou_00009@126.com>
2022-06-05 23:47:47 +0800

    update

--- lisp/init-lsp-bridge.el
+++ lisp/init-lsp-bridge.el
@@ -12,6 +12,15 @@
   (require 'lsp-bridge-orderless)
   (require 'lsp-bridge-jdtls)
   :config
+  (setq-local evil-goto-definition-functions '(lsp-bridge-jump))
+
+  (define-key evil-motion-state-map "gR" #'lsp-bridge-rename)
+  (define-key evil-motion-state-map "gr" #'lsp-bridge-find-references)
+  (define-key evil-normal-state-map "gi" #'lsp-bridge-find-impl)
+  (define-key evil-motion-state-map "gd" #'lsp-bridge-jump)
+  (define-key evil-motion-state-map "gs" #'lsp-bridge-restart-process)
+  (define-key evil-normal-state-map "gh" #'lsp-bridge-lookup-documentation)
+
   ;; 通过Cape融合不同的补全后端，比如lsp-bridge、 tabnine、 file、 dabbrev.
   (defun lsp-bridge-mix-multi-backends ()
     (setq-local completion-category-defaults nil)
