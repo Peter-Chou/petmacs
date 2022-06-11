2aec33243e371b65ddbc4e3f01f697a58a987b26
Peter-Chou <zhou_00009@126.com>
2022-06-11 21:57:24 +0800

    :lipstick:

--- lisp/init-eaf.el
+++ lisp/init-eaf.el
@@ -4,22 +4,32 @@
 ;; /usr/bin/python3 -m pip install pip setuptools --upgrade
 ;; python3 install install-eaf.py
 (use-package eaf
-  ;; :load-path "~/.emacs.d/site-lisp/emacs-application-framework"
   :load-path (lambda () (expand-file-name "site-lisp/emacs-application-framework" user-emacs-directory))
+  :custom
+  ;; See https://github.com/emacs-eaf/emacs-application-framework/wiki/Customization
+  (eaf-browser-continue-where-left-off t)
+  (eaf-browser-enable-adblocker t)
+  (browse-url-browser-function 'eaf-open-browser)
   :init
+  ;; add folders in app to load-path
   (let ((default-directory (expand-file-name "site-lisp/emacs-application-framework/app" user-emacs-directory)))
     (normal-top-level-add-subdirs-to-load-path))
   (require 'eaf)
+  (require 'eaf-browser)
+  (require 'eaf-image-viewer)
   (require 'eaf-jupyter)
   (require 'eaf-markdown-previewer)
-  (require 'eaf-pdf-viewer)
-  (require 'eaf-image-viewer)
   (require 'eaf-org-previewer)
-  (require 'eaf-browser)
+  (require 'eaf-pdf-viewer)
 
   (require 'eaf-all-the-icons)
   (require 'eaf-evil)
 
-  (setq eaf-python-command (expand-file-name "eaf/bin/python" (getenv "WORKON_HOME"))))
+  (setq eaf-python-command (expand-file-name "eaf/bin/python" (getenv "WORKON_HOME")))
+  :config
+  (defalias 'browse-web #'eaf-open-browser)
+  (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
+  )
 
 (provide 'init-eaf)
