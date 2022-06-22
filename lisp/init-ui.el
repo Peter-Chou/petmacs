d9985fce3c584c5ef2d43654d14041286b43771b
Peter-Chou <zhou_00009@126.com>
2022-06-22 10:43:38 +0800

    add 3d to modeline

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -67,7 +67,7 @@
 
    modus-themes-paren-match '(bold intense)
    ;; modus-themes-mode-line '(accented borderless (height . 0.9))
-   modus-themes-mode-line '(accented borderless)
+   modus-themes-mode-line '(accented borderless 3d)
    modus-themes-region '(bg-only)
    modus-themes-completions '((matches . (extrabold))
                               (selection . (semibold accented))
