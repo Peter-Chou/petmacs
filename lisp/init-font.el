ca3800cd47f7f8caa9f40a8073ff2d1f675740d3
Peter-Chou <zhou_00009@126.com>
2022-06-22 13:50:24 +0800

    add emoji font support

--- lisp/init-font.el
+++ lisp/init-font.el
@@ -13,14 +13,21 @@
 (if window-system
     (dolist (charset '(kana han cjk-misc bopomofo))
       (set-fontset-font (frame-parameter nil 'font) charset
-			;; (font-spec :family "Microsoft Yahei" :size 18.5))
-			(font-spec :family "等距更纱黑体 SC" :size petmacs-font-size))))
+			            ;; (font-spec :family "Microsoft Yahei" :size 18.5))
+			            (font-spec :family "等距更纱黑体 SC" :size petmacs-font-size))))
 
 
 ;; Specify font for all unicode characters
-(cl-loop for font in '("Apple Color Emoji" "Segoe UI Symbol" "Symbola" "Symbol")
+(cl-loop for font in '("Segoe UI Symbol" "Symbola" "Symbol")
          when (font-installed-p font)
-         return(set-fontset-font t 'unicode font nil 'prepend))
+         return (set-fontset-font t 'unicode font nil 'prepend))
+
+;; Emoji
+(cl-loop for font in '("Noto Color Emoji" "Apple Color Emoji")
+         when (font-installed-p font)
+         return (if (>= emacs-major-version 28)
+                    (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend)
+                  (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend)))
 
 ;; Fontify symbols representing faces with that face
 (use-package fontify-face)
