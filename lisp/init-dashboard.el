6a90927603ecc57b19bcc335c9d123e121ff5747
Peter-Chou <zhou_00009@126.com>
2022-06-22 10:57:27 +0800

    :lipstick:

--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -17,7 +17,7 @@
   :init
   (setq
    dashboard-banner-logo-title "Petmacs --- Adorable just like A PET"
-   dashboard-startup-banner (expand-file-name "resources/img/totoro_banner.png" user-emacs-directory)
+   dashboard-startup-banner (expand-file-name "resources/pics/totoro_banner.png" user-emacs-directory)
    dashboard-page-separator "\n\f\n"
    dashboard-center-content t
    dashboard-show-shortcuts nil
