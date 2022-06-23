a1ad892b7476fcbb6006d094391f25bf844513e3
Peter-Chou <zhou_00009@126.com>
2022-06-23 13:32:28 +0800

    update

--- init.el
+++ init.el
@@ -22,19 +22,19 @@
 
 (setq byte-compile-warnings '(cl-functions))
 
+(if (functionp 'json-serialize)
+    (message "Native JSON is available")
+  (message "Native JSON is *not* available"))
+
 (if (and (fboundp 'native-comp-available-p)
-	 (native-comp-available-p))
+	     (native-comp-available-p))
     (progn
       (message "Native compilation is available")
       ;; native-compile all Elisp files under a directory
       (native-compile-async (expand-file-name "site-lisp" user-emacs-directory) 'recursively)
       (setq package-native-compile t
-	    native-comp-async-report-warnings-errors nil))
+	        native-comp-async-report-warnings-errors nil))
   (message "Native complation is *not* available"))
-
-(if (functionp 'json-serialize)
-    (message "Native JSON is available")
-  (message "Native JSON is *not* available"))
 
 ;; Load path
 ;; Optimize: Force "lisp"" and "site-lisp" at the head to reduce the startup time.
