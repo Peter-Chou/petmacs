d80b9e3470f7b2776b8471a52ce738531d2887a1
Peter-Chou <zhou_00009@126.com>
2022-07-05 07:46:21 +0800

    :lipstick:

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -10,12 +10,12 @@
   "Display icons or not.")
 
 (defvar  petmacs-font
-  ;; "Monego Ligatures"
-  "Monego"
+  "Monego Ligatures"
+  ;; "Monego"
   ;; "MonegoLigatures Nerd Font"
   "font")
 
-(defvar petmacs-enable-ligatures nil
+(defvar petmacs-enable-ligatures t
   "enable ligatures")
 
 (defvar  petmacs-font-size 14.0
