f770f8b99259af0ceb293a550e66c9102cea02a7
Peter-Chou <zhou_00009@126.com>
2022-07-10 21:53:34 +0800

    update

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -14,7 +14,8 @@
   ;; (projectile-update-mode-line)         ; Update mode-line at the first time
 
   ;; Use the faster searcher to handle project files: ripgrep `rg'.
-  (when (executable-find "rg")
+  (when (and (not (executable-find "fd"))
+             (executable-find "rg"))
     (setq projectile-generic-command
           (let ((rg-cmd ""))
             (dolist (dir projectile-globally-ignored-directories)
@@ -23,11 +24,15 @@
 
   ;; Faster searching on Windows
   (when sys/win32p
-    (when (executable-find "rg")
-      (setq projectile-indexing-method 'alien)
-      (setq projectile-enable-caching nil))
+    (when (or (executable-find "fd") (executable-find "rg"))
+      (setq projectile-indexing-method 'alien
+            projectile-enable-caching nil))
 
     ;; FIXME: too slow while getting submodule files on Windows
-    (setq projectile-git-submodule-command nil)))
+    (setq projectile-git-submodule-command nil))
+
+  ;; Support Perforce project
+  (let ((val (or (getenv "P4CONFIG") ".p4config")))
+    (add-to-list 'projectile-project-root-files-bottom-up val)))
 
 (provide 'init-projectile)
