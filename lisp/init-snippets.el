80f8558483ca1c1b2263d28654dfb034591c12a3
Peter-Chou <zhou_00009@126.com>
2022-08-01 14:36:13 +0800

    :lipstick:

--- lisp/init-snippets.el
+++ lisp/init-snippets.el
@@ -2,7 +2,7 @@
 
 (use-package yasnippet
   :diminish yas-minor-mode
-  :bind ("C-M-e" . yas-expand)
+  :bind ("M-e" . yas-expand)
   :hook (after-init . yas-global-mode))
 
 (use-package yasnippet-snippets)
