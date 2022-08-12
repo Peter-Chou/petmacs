b90444fc2a73acb02ba923716194f74fc7a35b99
Peter-Chou <zhou_00009@126.com>
2022-08-13 07:47:44 +0800

    update

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -50,9 +50,7 @@
         (cond ((equal petmacs-lsp-client-mode 'lsp-mode)
                (lsp-deferred))
               ((equal petmacs-lsp-client-mode 'lsp-bridge-mode)
-               (if (bound-and-true-p lsp-bridge-mode)
-                   (lsp-bridge-restart-process)
-                 (lsp-bridge-mode 1)))))))
+               (lsp-bridge-restart-process))))))
   :hook (python-mode . petmacs/pyvenv-pyright-autoload)
   :config
   (pyvenv-mode 1)
