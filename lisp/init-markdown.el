d96a60e964a6ce2577762959881ff5b90aa39e24
Peter-Chou <zhou_00009@126.com>
2022-09-18 15:18:40 +0800

    update markdown

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,7 +1,6 @@
 ;; -*- lexical-binding: t no-byte-compile: t -*-
 
 (use-package markdown-mode
-  :hook ((markdown-mode . auto-fill-mode))
   :mode (("README\\.md\\'" . gfm-mode))
   :init
   (setq markdown-enable-wiki-links t
@@ -9,12 +8,79 @@
         markdown-asymmetric-header t
         markdown-make-gfm-checkboxes-buttons t
         markdown-gfm-uppercase-checkbox t
-        markdown-fontify-code-blocks-natively t)
-  )
+        markdown-fontify-code-blocks-natively t
+
+        markdown-content-type "application/xhtml+xml"
+        markdown-css-paths '("https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css"
+                             "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/github.min.css")
+        markdown-xhtml-header-content "
+<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+<style>
+body {
+  box-sizing: border-box;
+  max-width: 740px;
+  width: 100%;
+  margin: 40px auto;
+  padding: 0 10px;
+}
+</style>
+<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/default.min.css'>
+<script src='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/highlight.min.js'></script>
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.classList.add('markdown-body');
+  document.querySelectorAll('pre code').forEach((code) => {
+    if (code.className != 'mermaid') {
+      hljs.highlightBlock(code);
+    }
+  });
+});
+</script>
+<script src='https://unpkg.com/mermaid@8.4.8/dist/mermaid.min.js'></script>
+<script>
+mermaid.initialize({
+  theme: 'default',  // default, forest, dark, neutral
+  startOnLoad: true
+});
+</script>
+"
+        markdown-gfm-additional-languages "Mermaid")
+
+  ;; `multimarkdown' is necessary for `highlight.js' and `mermaid.js'
+  (when (executable-find "multimarkdown")
+    (setq markdown-command "multimarkdown"))
+  :config
+  ;; Support `mermaid'
+  (add-to-list 'markdown-code-lang-modes '("mermaid" . mermaid-mode))
+
+  (with-no-warnings
+    ;; Use `which-key' instead
+    (advice-add #'markdown--command-map-prompt :override #'ignore)
+    (advice-add #'markdown--style-map-prompt   :override #'ignore)))
 
 ;; Table of contents
 (use-package markdown-toc
+  :diminish
   :bind (:map markdown-mode-command-map
-         ("r" . markdown-toc-generate-or-refresh-toc)))
+         ("r" . markdown-toc-generate-or-refresh-toc))
+  :hook (markdown-mode . markdown-toc-mode)
+  :init (setq markdown-toc-indentation-space 2
+              markdown-toc-header-toc-title "\n## Table of Contents"
+              markdown-toc-user-toc-structure-manipulation-fn 'cdr)
+  :config
+  (with-no-warnings
+    (define-advice markdown-toc-generate-toc (:around (fn &rest args) lsp)
+      "Generate or refresh toc after disabling lsp."
+      (cond
+       ((bound-and-true-p lsp-managed-mode)
+        (lsp-managed-mode -1)
+        (apply fn args)
+        (lsp-managed-mode 1))
+       ((bound-and-true-p eglot--manage-mode)
+        (eglot--manage-mode -1)
+        (apply fn args)
+        (eglot--manage-mode 1))
+       (t
+        (apply fn args))))))
 
 (provide 'init-markdown)
