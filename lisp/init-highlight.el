aba54dda2d6eac982962816778ef7d2c45926d63
Peter-Chou <zhou_00009@126.com>
2023-08-24 14:14:44 +0800

    improve indent-bars

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -90,59 +90,15 @@ FACE defaults to inheriting from default and highlight."
   :quelpa (indent-bars :fetcher github
     	               :repo "jdtsmith/indent-bars"
     	               :files ("*"))
-  :hook ((python-mode yaml-mode) . indent-bars-mode)
+  :preface
+  (defface indent-bars-current-depth-line-face '((t nil))
+    "Face for indent bars current line"
+    :group 'basic-faces)
+  :hook ((python-mode python-ts-mode yaml-mode yaml-ts-mode) . indent-bars-mode)
   :init
+  (set-face-attribute 'indent-bars-current-depth-line-face nil :foreground petmacs-favor-color :bold t)
   (setq indent-bars-display-on-blank-lines nil
-        ;; indent-bars-highlight-current-depth petmacs-favor-color
-        )
-  :config
-  )
-
-;; Highlight indentions
-;; (use-package highlight-indent-guides
-;;   :diminish
-;;   :hook ((prog-mode yaml-mode) . (lambda ()
-;;                                    "Highlight indentations in small files for better performance."
-;;                                    (unless (too-long-file-p)
-;;                                      (highlight-indent-guides-mode 1))))
-;;   :init
-;;   (setq highlight-indent-guides-auto-enabled nil
-;;         highlight-indent-guides-method 'character
-;;         ;; highlight-indent-guides-character ?⸽
-;;         highlight-indent-guides-responsive 'top
-;;         highlight-indent-guides-suppress-auto-error t)
-
-;;   :config
-;;   (set-face-attribute 'highlight-indent-guides-character-face nil :foreground (color-lighten-name (face-foreground 'font-lock-type-face nil 'default) 20) :bold nil)
-;;   (set-face-attribute 'highlight-indent-guides-top-character-face nil :foreground petmacs-favor-color :bold t)
-;;   (with-no-warnings
-;;     ;; Don't display first level of indentation
-;;     (defun my-indent-guides-for-all-but-first-column (level responsive display)
-;;       (unless (< level 1)
-;;         (highlight-indent-guides--highlighter-default level responsive display)))
-;;     (setq highlight-indent-guides-highlighter-function
-;;           #'my-indent-guides-for-all-but-first-column)
-
-;;     ;; Disable in `macrostep' expanding
-;;     (with-eval-after-load 'macrostep
-;;       (advice-add #'macrostep-expand
-;;                   :after (lambda (&rest _)
-;;                            (when highlight-indent-guides-mode
-;;                              (highlight-indent-guides-mode -1))))
-;;       (advice-add #'macrostep-collapse
-;;                   :after (lambda (&rest _)
-;;                            (when (derived-mode-p 'prog-mode 'yaml-mode)
-;;                              (highlight-indent-guides-mode 1)))))))
-
-;; (use-package highlight-indentation
-;;   :hook ((prog-mode . highlight-indentation-mode)
-;;          (highlight-indentation-mode . highlight-indentation-current-column-mode))
-;;   :init (setq highlight-indentation-current-column-overlay-priority 2))
-
-;; (use-package indent-guide
-;;   :hook ((c-mode c++-mode cmake-mode python-mode emacs-lisp-mode) . indent-guide-mode)
-;;   :config
-;;   (set-face-attribute 'indent-guide-face nil :foreground petmacs-favor-color :bold t))
+        indent-bars-highlight-current-depth '(:face indent-bars-current-depth-line-face :pattern ".")))
 
 ;; Highlight brackets according to their depth
 (use-package rainbow-delimiters
