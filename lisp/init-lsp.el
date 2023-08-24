c4127efe78361d0c5f0ecdec7dddf284845f4bea
Peter-Chou <zhou_00009@126.com>
2023-08-24 09:41:13 +0800

    disable breadcrumb

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -66,7 +66,7 @@
         lsp-modeline-diagnostics-enable nil
         lsp-modeline-diagnostics-scope :workspace
         lsp-modeline-workspace-status-enable nil
-        lsp-headerline-breadcrumb-enable t
+        lsp-headerline-breadcrumb-enable nil
         lsp-headerline-breadcrumb-segments '(symbols)
 
         lsp-semantic-tokens-enable t
