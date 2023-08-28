88ee564be5efcbae8dc797bb44713c51851de41b
Peter-Chou <zhou_00009@126.com>
2023-08-28 09:50:28 +0800

    update

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -44,6 +44,60 @@
 
     (add-hook 'lsp-completion-mode-hook #'petmacs/lsp-mode-setup-completion)))
 
+;; M-x kind-icon-preview-all to reset and preview all icons after installation
+(use-package kind-icon
+  :quelpa (kind-icon :fetcher github
+  		             :repo "jdtsmith/kind-icon"
+  		             :files ("*.el"))
+  :when (icons-displayable-p)
+  :after corfu
+  :init
+  (require 'kind-icon)
+  ;; to compute blended backgrounds correctly
+  (setq ;; kind-icon-default-face 'corfu-default
+   kind-icon-use-icons nil
+   kind-icon-mapping
+   `(
+     (array ,(nerd-icons-codicon "nf-cod-symbol_array") :face font-lock-type-face)
+     (boolean ,(nerd-icons-codicon "nf-cod-symbol_boolean") :face font-lock-builtin-face)
+     (class ,(nerd-icons-codicon "nf-cod-symbol_class") :face font-lock-type-face)
+     (color ,(nerd-icons-codicon "nf-cod-symbol_color") :face success)
+     (command ,(nerd-icons-codicon "nf-cod-terminal") :face default)
+     (constant ,(nerd-icons-codicon "nf-cod-symbol_constant") :face font-lock-constant-face)
+     (constructor ,(nerd-icons-codicon "nf-cod-triangle_right") :face font-lock-function-name-face)
+     (enummember ,(nerd-icons-codicon "nf-cod-symbol_enum_member") :face font-lock-builtin-face)
+     (enum-member ,(nerd-icons-codicon "nf-cod-symbol_enum_member") :face font-lock-builtin-face)
+     (enum ,(nerd-icons-codicon "nf-cod-symbol_enum") :face font-lock-builtin-face)
+     (event ,(nerd-icons-codicon "nf-cod-symbol_event") :face font-lock-warning-face)
+     (field ,(nerd-icons-codicon "nf-cod-symbol_field") :face font-lock-variable-name-face)
+     (file ,(nerd-icons-codicon "nf-cod-symbol_file") :face font-lock-string-face)
+     (folder ,(nerd-icons-codicon "nf-cod-folder") :face font-lock-doc-face)
+     (interface ,(nerd-icons-codicon "nf-cod-symbol_interface") :face font-lock-type-face)
+     (keyword ,(nerd-icons-codicon "nf-cod-symbol_keyword") :face font-lock-keyword-face)
+     (macro ,(nerd-icons-codicon "nf-cod-symbol_misc") :face font-lock-keyword-face)
+     (magic ,(nerd-icons-codicon "nf-cod-wand") :face font-lock-builtin-face)
+     (method ,(nerd-icons-codicon "nf-cod-symbol_method") :face font-lock-function-name-face)
+     (function ,(nerd-icons-codicon "nf-cod-symbol_method") :face font-lock-function-name-face)
+     (module ,(nerd-icons-codicon "nf-cod-file_submodule") :face font-lock-preprocessor-face)
+     (numeric ,(nerd-icons-codicon "nf-cod-symbol_numeric") :face font-lock-builtin-face)
+     (operator ,(nerd-icons-codicon "nf-cod-symbol_operator") :face font-lock-comment-delimiter-face)
+     (param ,(nerd-icons-codicon "nf-cod-symbol_parameter") :face default)
+     (property ,(nerd-icons-codicon "nf-cod-symbol_property") :face font-lock-variable-name-face)
+     (reference ,(nerd-icons-codicon "nf-cod-references") :face font-lock-variable-name-face)
+     (snippet ,(nerd-icons-codicon "nf-cod-symbol_snippet") :face font-lock-string-face)
+     (string ,(nerd-icons-codicon "nf-cod-symbol_string") :face font-lock-string-face)
+     (struct ,(nerd-icons-codicon "nf-cod-symbol_structure") :face font-lock-variable-name-face)
+     (text ,(nerd-icons-codicon "nf-cod-text_size") :face font-lock-doc-face)
+     (typeparameter ,(nerd-icons-codicon "nf-cod-list_unordered") :face font-lock-type-face)
+     (type-parameter ,(nerd-icons-codicon "nf-cod-list_unordered") :face font-lock-type-face)
+     (unit ,(nerd-icons-codicon "nf-cod-symbol_ruler") :face font-lock-constant-face)
+     (value ,(nerd-icons-codicon "nf-cod-symbol_field") :face font-lock-builtin-face)
+     (variable ,(nerd-icons-codicon "nf-cod-symbol_variable") :face font-lock-variable-name-face)
+     (t ,(nerd-icons-codicon "nf-cod-code") :face font-lock-warning-face)))
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
+
 ;; Use dabbrev with Corfu!
 (use-package dabbrev
   ;; Swap M-/ and C-M-/
@@ -74,7 +128,11 @@
   ;; 默认用这三个补全后端
   (add-to-list 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-  (add-to-list 'completion-at-point-functions #'cape-keyword))
+  (add-to-list 'completion-at-point-functions #'cape-elisp-block)
+  (add-to-list 'completion-at-point-functions #'cape-abbrev)
+  (add-to-list 'completion-at-point-functions #'cape-keyword)
+
+  (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster))
 
 (unless (display-graphic-p)
   (use-package corfu-terminal
