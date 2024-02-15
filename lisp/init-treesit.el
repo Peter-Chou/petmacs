9caa31e7529d111d77a5236700360506d2ec9d44
Peter-Chou <2747244153@qq.com>
2024-02-15 19:27:17 +0800

    update treesit

--- lisp/init-treesit.el
+++ lisp/init-treesit.el
@@ -2,13 +2,39 @@
 
 
 (use-package treesit-auto
+  :demand t
   :init
-  (require 'treesit-auto)
+  ;; (require 'treesit-auto)
   (setq treesit-auto-install t
         treesit-font-lock-level 4)
   :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
+  ;; (treesit-auto-add-to-auto-mode-alist 'all)
+  ;; (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
+
+(use-package treesit
+  :ensure nil
+  :when (and (fboundp 'treesit-available-p)
+             (treesit-available-p))
+  :custom (major-mode-remap-alist
+           '((c-mode          . c-ts-mode)
+             (c++-mode        . c++-ts-mode)
+             (cmake-mode        . cmake-ts-mode)
+             (csharp-mode     . csharp-ts-mode)
+             (conf-toml-mode  . toml-ts-mode)
+             (css-mode        . css-ts-mode)
+             (yaml-mode        . yaml-ts-mode)
+             (java-mode       . java-ts-mode)
+             (javascript-mode . js-ts-mode)
+             (json-mode . json-ts-mode)
+             (js-json-mode    . json-ts-mode)
+             (python-mode     . python-ts-mode)
+             (ruby-mode       . ruby-ts-mode)
+             (sh-mode         . bash-ts-mode)))
+  :config
+  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode)))
 
 ;; (use-package treesit
 ;;   :ensure nil
