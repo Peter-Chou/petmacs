c391e7bb0cc93ab0308c53f2d1dfba7574c2b050
Peter-Chou <2747244153@qq.com>
2024-02-22 22:49:07 +0800

    set transparence default to 100

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -515,8 +515,8 @@
      ("=" transwin-inc "increase frame opacity")
      ("0" transwin-toggle "toggle frame opacity")
      ("o" transwin-ask "set frame opacity"))))
-  :hook (emacs-startup . (lambda ()
-                           (transwin-ask '90)))
+  ;; :hook (emacs-startup . (lambda ()
+  ;;                          (transwin-ask '90)))
   :bind
   ("M-+" . transwin-inc)
   ("M-_" . transwin-dec)
