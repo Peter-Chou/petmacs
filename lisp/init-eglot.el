45dc798ade03eb62c14e8f7051f95f72a2051509
Peter-Chou <2747244153@qq.com>
2024-03-09 21:56:39 +0800

    finish eglot-java

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -26,13 +26,12 @@
                                   ((bash-ts-mode sh-mode) . ("bash-language-server" "start"))
                                   ((go-mode go-dot-mod-mode go-dot-work-mode go-ts-mode go-mod-ts-mode) . ("gopls"))
 
-                                  ;; ((java-mode java-ts-mode) . ("jdtls"))
+                                  ((java-mode java-ts-mode) . ("jdtls"))
                                   ((yaml-ts-mode yaml-mode) . ("yaml-language-server" "--stdio"))
                                   ((dockerfile-mode dockerfile-ts-mode) . ("docker-langserver" "--stdio")))
           )
 
     :config
-    (require 'eglot)
     (push :documentHighlightProvider eglot-ignored-server-capabilities)
 
     ;; jdtls download see https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones
@@ -84,10 +83,28 @@
 (setq exec-path (append exec-path (list (expand-file-name ".emacs.d/share/eclipse.jdt.ls/bin"))))
 
 (use-package eglot-java
-  :hook ((java-mode java-ts-mode) . eglot-java-mode)
+  :hook ((java-mode java-ts-mode) . (lambda ()
+                                      (eglot-booster-mode t)
+                                      (eglot-java-mode t)))
   :init
+  ;; workaround
+  (defalias 'eglot-path-to-uri 'eglot--path-to-uri)
   (require 'eglot-java)
-  (setq eglot-java-java-home "/opt/jdk17"))
+  (setq eglot-java-java-home "/opt/jdk17"
+        eglot-java-eclipse-jdt-cache-directory (expand-file-name (file-name-concat user-emacs-directory "data" "eglot-java-cache"))
+        eglot-java-eclipse-jdt-args
+        `("-Xmx8G"
+          ,(concat "-javaagent:" (expand-file-name "~/.emacs.d/data/lombok-1.18.28.jar"))
+          "--add-modules=ALL-SYSTEM"
+          "--add-opens"
+          "java.base/java.util=ALL-UNNAMED"
+          "--add-opens"
+          "java.base/java.lang=ALL-UNNAMED"
+          "-XX:+UseZGC"
+          "-XX:+UseStringDeduplication"
+          ;; "-XX:FreqInlineSize=325"
+          ;; "-XX:MaxInlineLevel=9"
+          "-XX:+UseCompressedOops")))
 
 
 (use-package consult-eglot)
