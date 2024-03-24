6999118dd8c5b8df8d909613a0c6397c9154a390
Peter-Chou <2747244153@qq.com>
2024-03-24 21:52:32 +0800

    :lipstick:

--- lisp/init-workspace.el
+++ lisp/init-workspace.el
@@ -2,17 +2,17 @@
 
 (use-package tabspaces
   :hook (after-init . tabspaces-mode)
-  :custom
-  (tab-bar-show nil)
-
-  (tabspaces-use-filtered-buffers-as-default t)
-  (tabspaces-default-tab "Default")
-  (tabspaces-remove-to-default t)
-  (tabspaces-include-buffers '("*scratch*" "*Messages*"))
-  ;; sessions
-  (tabspaces-session t)
-  (tabspaces-session-auto-restore t)
   :init
+  (setq
+   ;; tab-bar-show nil
+   tabspaces-use-filtered-buffers-as-default t
+   tabspaces-default-tab "Default"
+   tabspaces-remove-to-default t
+   tabspaces-include-buffers '("*scratch*" "*Messages*")
+   ;; sessions
+   tabspaces-session t
+   tabspaces-session-auto-restore t)
+
   ;; Filter Buffers for Consult-Buffer
   (with-eval-after-load 'consult
     ;; hide full buffer list (still available with "b" prefix)
