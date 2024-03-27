abee6e01196e609b82683fe1300897d3aae0ce6d
Peter-Chou <2747244153@qq.com>
2024-03-27 20:31:26 +0800

    update

--- lisp/init-funcs.el
+++ lisp/init-funcs.el
@@ -404,7 +404,7 @@ This issue has been addressed in 28."
 
 (defun too-long-file-p ()
   "Check whether the file is too long."
-  (or (> (buffer-size) 100000)
+  (or (> (buffer-size) 500000)
       (and (fboundp 'buffer-line-statistics)
            (> (car (buffer-line-statistics)) 10000))))
 
