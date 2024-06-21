71070e404df2e82737d4a6c34de7e00d668971ca
Peter-Chou <2747244153@qq.com>
2024-06-21 22:30:48 +0800

    update

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -111,7 +111,6 @@
 ;; Environment
 (when (or sys/mac-x-p sys/linux-x-p (daemonp))
   (use-package exec-path-from-shell
-    :custom (exec-path-from-shell-arguments '("-l"))
     :config
     (dolist (var '("LSP_USE_PLISTS"))
       (add-to-list 'exec-path-from-shell-variables var)))
