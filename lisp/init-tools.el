8bca2ac81730518f8208c4513ab33912bbc4ab54
Peter-Chou <2747244153@qq.com>
2024-06-25 11:12:09 +0800

    update symbols-outline

--- lisp/init-tools.el
+++ lisp/init-tools.el
@@ -451,7 +451,7 @@
       (symbols-outline-show)))
   :after nerd-icons
   :init
-  (setq symbols-outline-window-position 'left
+  (setq symbols-outline-window-position 'right
         symbols-outline-use-nerd-icon-in-gui (not (image-type-available-p 'svg))
         symbols-outline-window-width 27
         symbols-outline-ignore-variable-symbols t
