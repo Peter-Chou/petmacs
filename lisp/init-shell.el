284fb537e761ccfa143364b00323237a43f07709
Peter-Chou <2747244153@qq.com>
2024-07-25 10:01:21 +0800

    :lipstick:

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -98,9 +98,9 @@
           (pop-to-buffer vterm-buffer)))
       (advice-add #'multi-vterm :override #'my-multi-vterm))
 
-    ;; update evil cursor bar / box in vterm
-    (with-eval-after-load 'evil
-      (advice-add #'vterm--redraw :after (lambda (&rest args) (evil-refresh-cursor evil-state))))
+    ;; update cursor bar / box in vterm
+    (advice-add #'vterm--redraw :around (lambda (fun &rest args)
+                                          (let ((cursor-type cursor-type)) (apply fun args))))
 
     (define-key vterm-mode-map [return]                      #'vterm-send-return)
     (setq vterm-keymap-exceptions nil)
