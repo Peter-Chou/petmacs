b4fb4775282e61ccd548212dd75d9b6850eaf200
Peter-Chou <2747244153@qq.com>
2025-02-25 22:09:49 +0800

    disable emojify

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -555,11 +555,11 @@ mouse-1: Display Line and Column Mode Menu")
           mini-frame-resize-max-height 25)))
 
 ;; disable install emojis when github is unavailable
-(use-package emojify
-  :hook (after-init . global-emojify-mode)
-  :init
-  (unless petmacs-quelpa-use-gitee-mirror
-    (setq emojify-download-emojis-p t)))
+;; (use-package emojify
+;;   :hook (after-init . global-emojify-mode)
+;;   :init
+;;   (unless petmacs-quelpa-use-gitee-mirror
+;;     (setq emojify-download-emojis-p t)))
 
 (use-package nyan-mode
   :hook (doom-modeline-mode . nyan-mode)
