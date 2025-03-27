4294921fd927e57569bdd2bb4a8ab3b900067872
Peter-Chou <2747244153@qq.com>
2025-03-27 17:25:52 +0800

    enable eglot inlay mode

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -28,8 +28,7 @@
         ;; eglot-connect-timeout 120
         eglot-connect-timeout 1200 ;; 10 minutes
         eglot-ignored-server-capabilities '(:documentHighlightProvider
-                                            :inlayHintProvider
-                                            :documentOnTypeFormattingProvider)
+                                            :documentOnTypeFormattingProvider) ;; :inlayHintProvider
         eldoc-echo-area-use-multiline-p nil
         ;; eglot-events-buffer-size 1
         eglot-server-programs
