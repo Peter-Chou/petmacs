2a63a3e1403ab0d6f2ea423cd1129867caa4c124
Peter-Chou <2747244153@qq.com>
2025-10-11 22:49:47 +0800

    update project

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -52,15 +52,6 @@
               (setq rg-cmd (format "%s --glob '!%s'" rg-cmd dir)))
             (concat "rg -0 --files --color=never --hidden" rg-cmd))))
 
-  ;; Faster searching on Windows
-  (when sys/win32p
-    (when (or (executable-find "fd") (executable-find "rg"))
-      (setq projectile-indexing-method 'alien
-            projectile-enable-caching nil))
-
-    ;; FIXME: too slow while getting submodule files on Windows
-    (setq projectile-git-submodule-command nil))
-
   ;; Support Perforce project
   (let ((val (or (getenv "P4CONFIG") ".p4config")))
     (add-to-list 'projectile-project-root-files-bottom-up val)))
