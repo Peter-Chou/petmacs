c6bc18e0c700f344e4e263fe47341832635736bf
Peter-Chou <2747244153@qq.com>
2026-01-13 09:20:49 +0800

    update elisp

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -10,16 +10,12 @@
 ;; Emacs lisp mode
 (use-package elisp-mode
   :ensure nil
-  :bind (:map emacs-lisp-mode-map
-         ("C-c C-x" . ielm)
-         ("C-c C-c" . eval-defun)
-         ("C-c C-b" . eval-buffer))
   :config
   ;; Syntax highlighting of known Elisp symbols
   (if (boundp 'elisp-fontify-semantically)
       (setq elisp-fontify-semantically t)
     (use-package highlight-defined
-      :hook ((emacs-lisp-mode inferior-emacs-lisp-mode) . highlight-defined-mode)))
+      :hook (emacs-lisp-mode inferior-emacs-lisp-mode)))
 
   (with-no-warnings
     ;; Align indent keywords
@@ -99,12 +95,24 @@ Lisp function does not specify a special indentation."
                                          indent-point normal-indent))
                   (method
                    (funcall method indent-point state))))))))
-    (add-hook 'emacs-lisp-mode-hook
-              (lambda () (setq-local lisp-indent-function #'my-lisp-indent-function)))
 
+    (setq lisp-indent-function #'my-lisp-indent-function)))
+
+;; Interactive macro expander
+(use-package macrostep
+  :bind (:map emacs-lisp-mode-map
+         ("C-c e" . macrostep-expand)
+         :map lisp-interaction-mode-map
+         ("C-c e" . macrostep-expand)))
+
+(use-package help-mode
+  :ensure nil
+  :hook (help-mode . cursor-sensor-mode)
+  :bind (:map help-mode-map
+         ("r" . remove-hook-at-point))
+  :config
+  (with-no-warnings
     ;; Add remove buttons for advices
-    (add-hook 'help-mode-hook 'cursor-sensor-mode)
-
     (defun function-advices (function)
       "Return FUNCTION's advices."
       (let ((flist (indirect-function function)) advices)
@@ -112,6 +120,12 @@ Lisp function does not specify a special indentation."
           (setq advices `(,@advices ,(advice--car flist)))
           (setq flist (advice--cdr flist)))
         advices))
+
+    (defun help--update ()
+      "Update the help buffer."
+      (if (eq major-mode 'helpful-mode)
+          (helpful-update)
+        (revert-buffer nil t)))
 
     (defun add-remove-advice-button (advice function)
       (when (and (functionp advice) (functionp function))
@@ -127,9 +141,7 @@ Lisp function does not specify a special indentation."
                      (when (yes-or-no-p msg)
                        (message "%s from function `%s'" msg function)
                        (advice-remove function advice)
-                       (if (eq major-mode 'helpful-mode)
-                           (helpful-update)
-                         (revert-buffer nil t))))
+                       (help--update)))
            'follow-link t))))
 
     (defun add-button-to-remove-advice (buffer-or-name function)
@@ -164,8 +176,8 @@ Lisp function does not specify a special indentation."
                (func (when (and
                             (or (re-search-forward (format "^Value:?[\s|\n]") nil t)
                                 (goto-char orig-point))
-                            (sexp-at-point))
-                       (end-of-sexp)
+                            (thing-at-point 'sexp))
+                       (thing-at-point--end-of-sexp)
                        (backward-char 1)
                        (catch 'break
                          (while t
@@ -174,20 +186,10 @@ Lisp function does not specify a special indentation."
                              (scan-error (throw 'break nil)))
                            (let ((bounds (bounds-of-thing-at-point 'sexp)))
                              (when (<= (car bounds) orig-point (cdr bounds))
-                               (throw 'break (sexp-at-point)))))))))
+                               (throw 'break (thing-at-point 'sexp)))))))))
             (when (yes-or-no-p (format "Remove %s from %s? " func hook))
-              (remove-hook hook func)
-              (if (eq major-mode 'helpful-mode)
-                  (helpful-update)
-                (revert-buffer nil t)))))))
-    (bind-key "r" #'remove-hook-at-point help-mode-map)))
-
-;; Interactive macro expander
-(use-package macrostep
-  :bind (:map emacs-lisp-mode-map
-         ("C-c e" . macrostep-expand)
-         :map lisp-interaction-mode-map
-         ("C-c e" . macrostep-expand)))
+              (remove-hook hook (intern func))
+              (help--update))))))))
 
 ;; A better *Help* buffer
 (use-package helpful
@@ -201,24 +203,21 @@ Lisp function does not specify a special indentation."
          :map lisp-interaction-mode-map
          ("C-c C-d"                 . helpful-at-point)
          :map helpful-mode-map
+         ("C-x K"                   . helpful-kill-buffers)
          ("r"                       . remove-hook-at-point))
-  :hook (helpful-mode . cursor-sensor-mode) ; for remove-advice button
-  :init
-  (with-no-warnings
-    (with-eval-after-load 'apropos
-      ;; patch apropos buttons to call helpful instead of help
-      (dolist (fun-bt '(apropos-function apropos-macro apropos-command))
-        (button-type-put
-         fun-bt 'action
-         (lambda (button)
-           (helpful-callable (button-get button 'apropos-symbol)))))
-      (dolist (var-bt '(apropos-variable apropos-user-option))
-        (button-type-put
-         var-bt 'action
-         (lambda (button)
-           (helpful-variable (button-get button 'apropos-symbol))))))))
-
-
+  :hook (helpful-mode . cursor-sensor-mode)
+  :init (with-eval-after-load 'apropos
+          ;; patch apropos buttons to call helpful instead of help
+          (dolist (fun-bt '(apropos-function apropos-macro apropos-command))
+            (button-type-put
+             fun-bt 'action
+             (lambda (button)
+               (helpful-callable (button-get button 'apropos-symbol)))))
+          (dolist (var-bt '(apropos-variable apropos-user-option))
+            (button-type-put
+             var-bt 'action
+             (lambda (button)
+               (helpful-variable (button-get button 'apropos-symbol)))))))
 
 (provide 'init-elisp)
 
